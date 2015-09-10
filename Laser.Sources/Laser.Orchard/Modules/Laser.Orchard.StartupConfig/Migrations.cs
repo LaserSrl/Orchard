7f83892eac7f05ccb53e7ed0c7122f6cbf2cb440
Lorenzo <lorenzo.frediani@laser-group.com>
2015-09-10 16:03:48 +0200

    Add inserita feature per gestione degli aggiornamenti

--- Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.StartupConfig/Migrations.cs
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.StartupConfig/Migrations.cs
@@ -1,44 +1,45 @@
 ﻿using Laser.Orchard.StartupConfig.Models;
+using Laser.Orchard.StartupConfig.Services;
 using Orchard.ContentManagement.MetaData;
-using Orchard.ContentManagement.MetaData.Models;
 using Orchard.Core.Contents.Extensions;
 using Orchard.Data.Migration;
 using Orchard.Environment.Extensions;
-using System.Linq;
 
 namespace Laser.Orchard.StartupConfig {
+
     public class MigrationStartupConfig : DataMigrationImpl {
 
         public int Create() {
-
             ContentDefinitionManager.AlterPartDefinition("CommonPart", b => b
                                                             .WithField("Creator", cfg => cfg.OfType("NumericField").WithDisplayName("Id of user Creator"))
                                                             .WithField("LastModifier", cfg => cfg.OfType("NumericField").WithDisplayName("Id of last user that have modified the content item"))
                                                             );
             return 1;
         }
+
         public int UpdateFrom1() {
             ContentDefinitionManager.AlterPartDefinition("PublishExtensionPart", b => b
                 .Attachable()
                 .WithField("PublishExtensionStatus", cfg => cfg.OfType("EnumerationField")
-                    .WithSetting("EnumerationFieldSettings.Required","true")
-                    .WithSetting("EnumerationFieldSettings.ListMode","Dropdown")
+                    .WithSetting("EnumerationFieldSettings.Required", "true")
+                    .WithSetting("EnumerationFieldSettings.ListMode", "Dropdown")
                     .WithSetting("EnumerationFieldSettings.Options", "Created\r\nLoaded\r\nAccepted\r\nRejected")
                     .WithDisplayName("Status"))
-                    
+
                 );
 
             return 2;
         }
     }
+
     [OrchardFeature("Laser.Orchard.StartupConfig.PermissionExtension")]
     public class MigrationsUsersGroups : DataMigrationImpl {
+
         public int Create() {
             return 1;
         }
 
         public int UpdateFrom1() {
-
             //ContentDefinitionManager.AlterPartDefinition("UsersGroupsPart", part => part.Attachable());
             ContentDefinitionManager.AlterTypeDefinition("User",
                 cfg => cfg
@@ -54,7 +55,6 @@ namespace Laser.Orchard.StartupConfig {
                        .Column<string>("theUserGroup")
            );
 
-
             SchemaBuilder.CreateTable("ExtendedUsersGroupsRecord",
         table =>
 
@@ -69,7 +69,6 @@ namespace Laser.Orchard.StartupConfig {
             SchemaBuilder.AlterTable("ExtendedUsersGroupsRecord", table => table.AlterColumn("GroupName", col => col.WithType(System.Data.DbType.String).WithLength(150)));
             return 3;
         }
-
     }
 
     /// <summary>
@@ -77,6 +76,7 @@ namespace Laser.Orchard.StartupConfig {
     /// </summary>
     [OrchardFeature("Laser.Orchard.StartupConfig.TaxonomiesExtensions")]
     public class MigrationTaxonomies : DataMigrationImpl {
+
         public int Create() {
             ContentDefinitionManager.AlterTypeDefinition("Taxonomy",
             cfg => cfg
@@ -84,7 +84,66 @@ namespace Laser.Orchard.StartupConfig {
             );
             return 1;
         }
-
     }
 
+    [OrchardFeature("Laser.Orchard.StartupConfig.Maintenance")]
+    public class MigrationMaintenance : DataMigrationImpl {
+        private readonly IUtilsServices _utilsServices;
+
+        public MigrationMaintenance(IUtilsServices utilsServices) {
+            _utilsServices = utilsServices;
+        }
+
+        public int Create() {
+            _utilsServices.EnableFeature("Orchard.PublishLater");
+            ContentDefinitionManager.AlterPartDefinition("MaintenancePart", b => b
+                .Attachable(false));
+            ContentDefinitionManager.AlterTypeDefinition("Maintenance", cfg => cfg
+                .WithPart("CommonPart")
+                .WithPart(typeof(MaintenancePart).Name)
+                .WithPart("PublishLaterPart")
+                .Creatable(true)
+                );
+            SchemaBuilder.CreateTable("MaintenancePartRecord", table => table
+                .ContentPartVersionRecord()
+                .Column<string>("MaintenanceNotifyType")
+                .Column<string>("MaintenanceNotify")
+            );
+            return 1;
+        }
+
+        public int UpdateFrom1() {
+            return 2;
+        }
+
+        public int UpdateFrom2() {
+            return 3;
+        }
+
+        public int UpdateFrom3() {
+            return 4;
+        }
+
+        public int UpdateFrom4() {
+            //ContentDefinitionManager.AlterTypeDefinition("Maintenance", b => b
+            //         .Draftable()
+            //         );
+            return 5;
+        }
+
+        public int UpdateFrom5() {
+            //SchemaBuilder.CreateTable("MaintenancePartRecord", table => table
+            //                .ContentPartVersionRecord()
+            //                .Column<string>("MaintenanceNotifyType")
+            //                .Column<string>("MaintenanceNotify")
+            //    );
+            return 6;
+        }
+
+        public int UpdateFrom6() {
+            //      SchemaBuilder.DropTable("MaintenancePartRecord");
+
+            return 7;
+        }
+    }
 }
---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.StartupConfig/Services/MaintenanceService.cs
@@ -0,0 +1,32 @@
+﻿using Laser.Orchard.StartupConfig.Models;
+using Laser.Orchard.StartupConfig.ViewModels;
+using Orchard;
+using Orchard.ContentManagement;
+using Orchard.Environment.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+
+namespace Laser.Orchard.StartupConfig.Services {
+    [OrchardFeature("Laser.Orchard.StartupConfig.Maintenance")]
+    public class MaintenanceService : IMaintenanceService {
+        private readonly IOrchardServices _orchardServices;
+
+        public MaintenanceService(IOrchardServices orchardServices) {
+            _orchardServices = orchardServices;
+        }
+        public List<MaintenanceVM> Get(){
+            var listofcontentitems = _orchardServices.ContentManager.Query<MaintenancePart>(VersionOptions.Published).List();
+            List<MaintenanceVM> ListMaintenanceVM = new List<MaintenanceVM>();
+            foreach (var y in listofcontentitems) {
+                MaintenanceVM MaintenanceVM = new MaintenanceVM();
+                Mapper.CreateMap<MaintenancePart, MaintenanceVM>();
+                Mapper.Map(y.As<MaintenancePart>(), MaintenanceVM);
+               ListMaintenanceVM.Add( MaintenanceVM);
+            }
+            return ListMaintenanceVM;
+        }
+    }
+}
