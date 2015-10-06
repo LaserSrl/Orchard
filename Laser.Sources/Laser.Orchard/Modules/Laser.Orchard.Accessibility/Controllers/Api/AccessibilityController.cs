6db85c6bca570cb219e2bfb64f97ba9d8a5dd815
marco.viglione <marco.viglione@laser-group.com>
2015-10-06 12:12:37 +0200

    Traduzioni Questionnaires
    - fix per apice nei messaggi javascript

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.Accessibility/Controllers/Api/AccessibilityController.cs
@@ -0,0 +1,38 @@
+﻿using Laser.Orchard.Accessibility.Services;
+using Orchard;
+using System.Web.Http;
+
+namespace Laser.Orchard.Accessibility.Controllers.Api
+{
+    public class AccessibilityController : ApiController
+    {
+        private readonly IOrchardServices _orchardServices;
+        private readonly IAccessibilityServices _accessibilityServices;
+
+        public AccessibilityController(IOrchardServices services, IAccessibilityServices acServices)
+            : base()
+        {
+            _orchardServices = services;
+            _accessibilityServices = acServices;
+        }
+
+        public IHttpActionResult Get()
+        {
+            string operation = _orchardServices.WorkContext.HttpContext.Request.QueryString.ToString();
+            switch (operation)
+            {
+                case "txt":
+                    _accessibilityServices.SetTextOnly();
+                    break;
+                case "normal":
+                    _accessibilityServices.SetNormal();
+                    break;
+                case "high":
+                    _accessibilityServices.SetHighContrast();
+                    break;
+            }
+
+            return Ok(operation);
+        }
+    }
+}
