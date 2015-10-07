c690c7d1e2a5ff294b4909b13cef60ea8c5fc853
marco.viglione <marco.viglione@laser-group.com>
2015-10-07 10:21:15 +0200

    Laser.Orchard.Accessibility
    - aggiunto controller per il test

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.Accessibility/Controllers/AccessibilityController.cs
@@ -0,0 +1,40 @@
+﻿using Laser.Orchard.Accessibility.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+
+namespace Laser.Orchard.Accessibility.Controllers
+{
+    public class AccessibilityController : Controller
+    {
+        private IAccessibilityServices _accessibilityServices;
+
+        public AccessibilityController(IAccessibilityServices acServices)
+        {
+            _accessibilityServices = acServices;
+        }
+        //
+        // GET: /Accessibility/
+        public ActionResult Index()
+        {
+            string operation = System.Web.HttpContext.Current.Request.QueryString.ToString();
+
+            switch (operation)
+            {
+                case "txt":
+                    _accessibilityServices.SetTextOnly();
+                    break;
+                case "high":
+                    _accessibilityServices.SetHighContrast();
+                    break;
+                case "normal":
+                    _accessibilityServices.SetNormal();
+                    break;
+            }
+            return Redirect("~/"); // RedirectToAction("Index", "Index");
+        }
+	}
+}
