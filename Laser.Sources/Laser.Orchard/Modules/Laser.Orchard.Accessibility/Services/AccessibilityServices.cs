57e4bfe0003245e00253eaf73a165a13a7845965
marco.viglione <marco.viglione@laser-group.com>
2015-10-07 09:33:52 +0200

    Laser.Orchard.Accessibility
    - prima versione

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.Accessibility/Services/AccessibilityServices.cs
@@ -0,0 +1,67 @@
+﻿using Orchard;
+using Orchard.Environment.Configuration;
+using System;
+using System.Web;
+
+namespace Laser.Orchard.Accessibility.Services
+{
+    public class AccessibilityServices : IAccessibilityServices
+    {
+        private readonly IOrchardServices _orchardServices;
+        private readonly ShellSettings _shellSettings;
+
+        public AccessibilityServices(IOrchardServices orchardServices, ShellSettings shellSettings)
+        {
+            _orchardServices = orchardServices;
+            _shellSettings = shellSettings;
+        }
+
+        private void setCookie(string cookieValue)
+        {
+            // calcola il path corretto per il cookie
+            string path = "/"; // path del cookie
+            string tenantPath = _shellSettings.RequestUrlPrefix ?? "";
+            string operation = _orchardServices.WorkContext.HttpContext.Request.QueryString.ToString();
+            string appPath = _orchardServices.WorkContext.HttpContext.Request.ApplicationPath;
+
+            if (tenantPath == "")
+            {
+                path = appPath;
+            }
+            else
+            {
+                path = appPath + "/" + tenantPath;
+            }
+
+            // setta il cookie
+            HttpCookie cook = new HttpCookie(Utils.AccessibilityCookieName);
+            cook.Path = path;
+            cook.Value = cookieValue;
+            if (cookieValue == "")
+            {
+                // elimina il cookie
+                cook.Expires = DateTime.UtcNow.AddMonths(-1);
+            }
+            else
+            {
+                cook.Expires = DateTime.UtcNow.AddMonths(1);
+            }
+            _orchardServices.WorkContext.HttpContext.Response.SetCookie(cook);
+        }
+
+        public void SetTextOnly()
+        {
+            setCookie(Utils.AccessibilityTextOnly);
+        }
+
+        public void SetNormal()
+        {
+            setCookie(Utils.AccessibilityNormal);
+        }
+
+        public void SetHighContrast()
+        {
+            setCookie(Utils.AccessibilityHighContrast);
+        }
+    }
+}
---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.Accessibility/Utils.cs
@@ -0,0 +1,42 @@
+﻿using System.Web;
+
+namespace Laser.Orchard.Accessibility
+{
+    public class Utils
+    {
+        public const string AccessibilityCookieName = "Accessibility";
+        public const string AccessibilityTextOnly = "textonly";
+        public const string AccessibilityNormal = "";
+        public const string AccessibilityHighContrast = "highcontrast";
+
+        /// <summary>
+        /// Get the value of the cookie with the specified name.
+        /// If there is more than one cookie with that name, the first one is returned.
+        /// Usually, this is the more specialized cookie in terms of domain and path.
+        /// This is useful if you want a tenant-specific cookie.
+        /// </summary>
+        /// <param name="cookieName"></param>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public string getTenantCookieValue(string cookieName, HttpRequestBase request)
+        {
+            string result = "";
+            HttpCookie cookie = null;
+
+            // cicla perché potrebbero esserci più cookie con lo stesso nome e path differente
+            for (int i = 0; i < request.Cookies.Count; i++)
+            {
+                cookie = request.Cookies[i];
+                if (cookie.Name == cookieName)
+                {
+                    result = cookie.Value;
+                    // esce dopo aver trovato il primo cookie col nome cercato perché dovrebbe
+                    // essere quello più specifico come dominio e path, quindi specifico del tenant
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
