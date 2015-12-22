a3a1d906fa98b60a4585e957db4f8971413c45b0
HermesSbicego-Laser <hermes.sbicego@laser-group.com>
2015-12-22 12:53:45 +0100

    CommunicationGateway - Controlli validità campagna

--- Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.CommunicationGateway/ViewModels/ContentIndexVM.cs
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.CommunicationGateway/ViewModels/ContentIndexVM.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Orchard.ContentManagement;
 
 namespace Laser.Orchard.CommunicationGateway.ViewModels {
     public class ContentIndexVM {
@@ -10,6 +11,6 @@ namespace Laser.Orchard.CommunicationGateway.ViewModels {
         public string UserName { get; set; }
         public DateTime? ModifiedUtc { get; set; }
         public dynamic Option { get; set; }
-
+        public ContentItem ContentItem { get; set; }
     }
 }
