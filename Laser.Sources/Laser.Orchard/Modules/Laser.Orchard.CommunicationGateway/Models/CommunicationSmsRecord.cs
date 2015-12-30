04d30258cdc42858e61ad55df97d634922c50fbb
Lorenzo <lorenzo.frediani@laser-group.com>
2015-12-30 16:27:43 +0100

    fixed

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.CommunicationGateway/Models/CommunicationSmsRecord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Laser.Orchard.CommunicationGateway.Models {
+
+    public class CommunicationSmsRecord {
+        public virtual int Id { get; set; }
+        public virtual int CommunicationContactPartRecord_Id { get; set; }
+        public virtual string Language { get; set; }
+        public virtual bool Validated { get; set; }
+        public virtual DateTime DataInserimento { get; set; }
+        public virtual DateTime DataModifica { get; set; }
+        public virtual string Sms { get; set; }
+        public virtual string Prefix { get; set; }
+        public virtual bool Produzione { get; set; }
+
+        public CommunicationSmsRecord() {
+            this.CommunicationContactPartRecord_Id = 0;
+            this.Id = 0;
+            this.Validated = true;
+            this.DataInserimento = DateTime.Now;
+            this.DataModifica = DateTime.Now;
+            this.Produzione = true;
+        }
+    }
+}
---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.CommunicationGateway/ViewModels/View_EmailVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laser.Orchard.CommunicationGateway.ViewModels {
+    public class View_EmailVM {
+        public List<View_EmailVM_element> Elenco { get; set; }
+        public View_EmailVM() {
+            Elenco = new List<View_EmailVM_element>();
+        }
+    }
+    public class View_EmailVM_element {
+        public int Id { get; set; }
+        public int CommunicationContactPartRecord_Id { get; set; }
+        public string Language { get; set; }
+        public bool Validated { get; set; }
+        public DateTime DataInserimento { get; set; }
+        public DateTime DataModifica { get; set; }
+        public string Email { get; set; }
+        public bool Produzione { get; set; }
+    }
+}
