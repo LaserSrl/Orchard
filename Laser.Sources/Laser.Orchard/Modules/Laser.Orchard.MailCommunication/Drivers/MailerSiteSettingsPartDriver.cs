1ae0ec901aa806bd93a20903c6dfffae22f627ff
giuseppe.musso <giuseppe.musso@laser-group.com>
2016-01-19 12:13:35 +0100

    MailCommunication: - aggiunti i file mancanti su Git

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.MailCommunication/Drivers/MailerSiteSettingsPartDriver.cs
@@ -0,0 +1,37 @@
+﻿using Laser.Orchard.MailCommunication.Models;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Drivers;
+using Orchard.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laser.Orchard.MailCommunication.Drivers
+{
+    public class MailerSiteSettingsPartDriver : ContentPartDriver<MailerSiteSettingsPart>
+    {
+        public MailerSiteSettingsPartDriver()
+        {
+            T = NullLocalizer.Instance;
+        }
+
+        public Localizer T { get; set; }
+
+        protected override string Prefix { get { return "MailerSettings"; } }
+
+        protected override DriverResult Editor(MailerSiteSettingsPart part, dynamic shapeHelper) {
+            return Editor(part, null, shapeHelper);
+        }
+
+        protected override DriverResult Editor(MailerSiteSettingsPart part, IUpdateModel updater, dynamic shapeHelper) {
+            return ContentShape("Parts_MailerSiteSettings_Edit", () => {
+                if (updater != null) {
+                    updater.TryUpdateModel(part, Prefix, null, null);
+                }
+                return shapeHelper.EditorTemplate(TemplateName: "Parts/MailerSiteSettings", Model: part, Prefix: Prefix);
+            })
+                .OnGroup("Mailer");
+        }
+    }
+}
