662e6f624e05b710daacc9b41e437bd94795098c
Lorenzo <lorenzo.frediani@laser-group.com>
2016-01-20 11:47:43 +0100

    Add ShareLink Module usato dal mobile per condividere contenuti usando link,testo e immagine

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.ShareLink/Drivers/ShareLinkPartDriver.cs
@@ -0,0 +1,138 @@
+﻿using AutoMapper;
+using Laser.Orchard.ShareLink.Models;
+using Laser.Orchard.ShareLink.PartSettings;
+using Laser.Orchard.ShareLink.ViewModels;
+using Orchard;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Drivers;
+using Orchard.Localization;
+using Orchard.Logging;
+using Orchard.MediaLibrary.Models;
+using Orchard.Mvc.Extensions;
+using Orchard.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Laser.Orchard.ShareLink.Drivers {
+
+    public class ShareLinkPartDriver : ContentPartDriver<ShareLinkPart> {
+        private readonly ITokenizer _tokenizer;
+        private readonly IOrchardServices _orchardServices;
+
+        public ILogger Logger { get; set; }
+        public Localizer T { get; set; }
+
+        protected override string Prefix {
+            get { return "Laser.Orchard.ShareLink"; }
+        }
+
+        public ShareLinkPartDriver(IOrchardServices orchardServices, ITokenizer tokenizer) {
+            _orchardServices = orchardServices;
+            Logger = NullLogger.Instance;
+            T = NullLocalizer.Instance;
+            _tokenizer = tokenizer;
+        }
+
+        protected override DriverResult Editor(ShareLinkPart part, dynamic shapeHelper) {
+            var urlHelper = new UrlHelper(_orchardServices.WorkContext.HttpContext.Request.RequestContext);
+
+            ShareLinkVM vm = new ShareLinkVM();
+            Mapper.CreateMap<ShareLinkPart, ShareLinkVM>();
+            Mapper.Map(part, vm);
+            var moduleSetting = _orchardServices.WorkContext.CurrentSite.As<ShareLinkModuleSettingPart>();
+            var partSetting = part.Settings.GetModel<ShareLinkPartSettingVM>();
+            var tokens = new Dictionary<string, object> { { "Content", part.ContentItem } };
+            if ((!partSetting.ShowTextChoise) || part.SharedText == "") {
+                if (!string.IsNullOrEmpty(partSetting.SharedText)) {
+                    vm.SharedText = _tokenizer.Replace(partSetting.SharedText, tokens);
+                    vm.ShowSharedText = false;
+                }
+                else {
+                    if (!string.IsNullOrEmpty(moduleSetting.SharedText)) {
+                        vm.SharedText = _tokenizer.Replace(moduleSetting.SharedText, tokens);
+                        vm.ShowSharedText = false;
+                    }
+                }
+            }
+            if (partSetting.ShowTextChoise) {
+                vm.ShowSharedText = true;
+            }
+
+            if ((!partSetting.ShowLinkChoise) || part.SharedLink == "") {
+                if (!string.IsNullOrEmpty(partSetting.SharedLink)) {
+                    vm.SharedLink = _tokenizer.Replace(partSetting.SharedLink, tokens);
+                    vm.ShowSharedLink = false;
+                }
+                else {
+                    if (!string.IsNullOrEmpty(moduleSetting.SharedLink)) {
+                        vm.SharedLink = _tokenizer.Replace(moduleSetting.SharedLink, tokens);
+                        vm.ShowSharedLink = false;
+                    }
+                }
+            }
+            if (partSetting.ShowLinkChoise) {
+                vm.ShowSharedLink = true;
+            }
+
+            string ListId = "";
+            if (!string.IsNullOrEmpty(partSetting.SharedImage)) {
+                ListId = _tokenizer.Replace(partSetting.SharedImage, tokens);
+                vm.SharedImage = getimgurl(ListId);
+                vm.ShowSharedImage = false;
+            }
+            else {
+                if (!string.IsNullOrEmpty(moduleSetting.SharedImage)) {
+                    ListId = _tokenizer.Replace(moduleSetting.SharedImage, tokens);
+                    vm.SharedImage = getimgurl(ListId);
+                    vm.ShowSharedImage = false;
+                }
+            }
+
+            if (partSetting.ShowImageChoise) {
+                vm.ShowSharedImage = true;
+
+                List<OptionList> optionList = new List<OptionList>();
+                var mylist = ListId.Replace("{", "").Replace("}", "").Split(',');
+                foreach (string myid in mylist) {
+                    //  lSelectList.Insert(0, new SelectListItem() { Value = fa.Id.ToString(), Text = fa.AccountType + " - " + fa.DisplayAs });
+                    OptionList ol = new OptionList {
+                        Value = myid,
+                        Text = "",
+                        ImageUrl = getimgurl(myid),
+                        Selected = part.SharedIdImage == myid ? "selected=\"selected\"" : ""
+                    };
+                    optionList.Add(ol);
+                }
+                vm.ListOption = optionList;
+            }
+
+            return ContentShape("Parts_ShareLink",
+                                () => shapeHelper.EditorTemplate(TemplateName: "Parts/ShareLink",
+                                    Model: vm,
+                                    Prefix: Prefix));
+        }
+
+        protected override DriverResult Editor(ShareLinkPart part, IUpdateModel updater, dynamic shapeHelper) {
+            ShareLinkVM vm = new ShareLinkVM();
+            updater.TryUpdateModel(vm, Prefix, null, null);
+            Mapper.CreateMap<ShareLinkVM, ShareLinkPart>();
+            Mapper.Map(vm, part);
+            part.SharedIdImage = vm.SharedImage.Replace("{", "").Replace("}", "");
+            part.SharedImage = getimgurl(part.SharedIdImage);
+            return Editor(part, shapeHelper);
+        }
+
+        private string getimgurl(string idimg) {
+            var urlHelper = new UrlHelper(_orchardServices.WorkContext.HttpContext.Request.RequestContext);
+            Int32 idimage = 0;
+            Int32.TryParse(idimg.Replace("{", "").Replace("}", "").Split(',')[0], out idimage); ;
+            if (idimage > 0) {
+                var ContentImage = _orchardServices.ContentManager.Get(idimage, VersionOptions.Published);
+                return urlHelper.MakeAbsolute(ContentImage.As<MediaPart>().MediaUrl);
+            }
+            else
+                return idimg; // non ho passato un id e quindi sarà un link
+        }
+    }
+}
---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.ShareLink/PartSettings/ShareLinkPartSettingVM.cs
@@ -0,0 +1,11 @@
+﻿namespace Laser.Orchard.ShareLink.PartSettings {
+
+    public class ShareLinkPartSettingVM {
+        public string SharedLink { get; set; }
+        public string SharedText { get; set; }
+        public string SharedImage { get; set; }
+        public bool ShowImageChoise { get; set; }
+        public bool ShowTextChoise { get; set; }
+        public bool ShowLinkChoise { get; set; }
+    }
+}
