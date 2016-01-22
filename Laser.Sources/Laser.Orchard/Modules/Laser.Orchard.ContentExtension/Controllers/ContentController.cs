749784a284f60559af3f1d643301ea24b62eda53
Lorenzo <lorenzo.frediani@laser-group.com>
2016-01-22 16:27:55 +0100

    Add Get method to get enumerators and taxonomies

--- Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.ContentExtension/Controllers/ContentController.cs
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.ContentExtension/Controllers/ContentController.cs
@@ -3,13 +3,19 @@ using Laser.Orchard.StartupConfig.ViewModels;
 using Orchard;
 using Orchard.Autoroute.Services;
 using Orchard.ContentManagement;
+using Orchard.ContentManagement.MetaData;
+using Orchard.ContentManagement.MetaData.Models;
 using Orchard.Environment.Configuration;
+using Orchard.Fields.Fields;
 using Orchard.Localization;
 using Orchard.Localization.Models;
 using Orchard.Localization.Services;
 using Orchard.Logging;
+using Orchard.MediaLibrary.Fields;
 using Orchard.Security;
-using RazorEngine;
+using Orchard.Taxonomies.Fields;
+using Orchard.Taxonomies.Models;
+using Orchard.Taxonomies.Services;
 using RazorEngine.Compilation;
 using RazorEngine.Compilation.ReferenceResolver;
 using RazorEngine.Configuration;
@@ -18,12 +24,10 @@ using System;
 using System.Collections.Generic;
 using System.Dynamic;
 using System.IO;
-using System.Reflection;
+using System.Linq;
 using System.Web.Hosting;
 using System.Web.Http;
 using OrchardCore = Orchard.Core;
-using System.Linq;
-
 
 namespace Laser.Orchard.ContentExtension.Controllers {
 
@@ -35,49 +39,187 @@ namespace Laser.Orchard.ContentExtension.Controllers {
         private readonly ILocalizationService _localizationService;
         private readonly ICultureManager _cultureManager;
         private readonly ShellSettings _shellSettings;
-
+        private readonly IContentDefinitionManager _contentDefinitionManager;
+        private readonly ITaxonomyService _taxonomyService;
         public ILogger Logger { get; set; }
-
-        //    private readonly ITaxonomyService _taxonomyService;
         private readonly IContentExtensionsServices _contentExtensionsServices;
-
+        private readonly ILocalizedStringManager _localizedStringManager;
         private readonly IUtilsServices _utilsServices;
-
         public Localizer T { get; set; }
 
-        //       private readonly ILocalizedStringManager _localizedStringManager;
-
-        //     public ILogger Log { get; set; }
-
         public ContentController(
-            ShellSettings shellSettings,
-            ICsrfTokenHelper csrfTokenHelper,
-            IOrchardServices orchardServices,
-            //    ITaxonomyService taxonomyService,
-            //    ILocalizedStringManager localizedStringManager,
-            IAuthenticationService authenticationService,
-            IContentExtensionsServices contentExtensionsServices,
-            Lazy<IAutorouteService> autorouteService,
-            ILocalizationService localizationService,
-            ICultureManager cultureManager,
-            IUtilsServices utilsServices
-            ) {
+           ShellSettings shellSettings,
+           ICsrfTokenHelper csrfTokenHelper,
+           IOrchardServices orchardServices,
+           IAuthenticationService authenticationService,
+           IContentExtensionsServices contentExtensionsServices,
+           Lazy<IAutorouteService> autorouteService,
+           ILocalizationService localizationService,
+           ICultureManager cultureManager,
+           IUtilsServices utilsServices,
+           IContentDefinitionManager contentDefinitionManager,
+            ITaxonomyService taxonomyService,
+           ILocalizedStringManager localizedStringManager
+           ) {
+            _localizedStringManager = localizedStringManager;
+            _taxonomyService = taxonomyService;
+            _contentDefinitionManager = contentDefinitionManager;
             _shellSettings = shellSettings;
             _csrfTokenHelper = csrfTokenHelper;
             _orchardServices = orchardServices;
-            //      _taxonomyService = taxonomyService;
             T = NullLocalizer.Instance;
-            //     _localizedStringManager = localizedStringManager;
-            //       Log = NullLogger.Instance;
             _authenticationService = authenticationService;
             _contentExtensionsServices = contentExtensionsServices;
             _autorouteService = autorouteService;
             _localizationService = localizationService;
             _cultureManager = cultureManager;
             _utilsServices = utilsServices;
-
-            //  _context = context;
             Logger = NullLogger.Instance;
+        }
+
+        /// <summary>
+        /// esempio http://localhost/Laser.Orchard/expoincitta/api/Laser.Orchard.ContentExtension/Content?ContentType=User
+        /// da richiamare come application/json e non come form
+        /// </summary>
+        /// <param name="ContentType"></param>
+        /// <param name="Language"></param>
+        /// <returns></returns>
+        public dynamic Get(string ContentType, string Language = "it-IT") {
+            //var currentUser = _authenticationService.GetAuthenticatedUser();
+            //if (currentUser == null){
+            //       return (_utilsServices.GetResponse(ResponseType.InvalidUser));// { Message = "Error: No current User", Success = false,ErrorCode=ErrorCode.InvalidUser,ResolutionAction=ResolutionAction.Login });
+            //}
+            ContentTypeDefinition contentTypeDefinition = _contentDefinitionManager.GetTypeDefinition(ContentType);
+            var eObj = new ExpandoObject() as IDictionary<string, Object>;
+
+            #region Tutti i field
+
+            foreach (ContentTypePartDefinition ctpd in contentTypeDefinition.Parts) {
+                // var fields = contentTypeDefinition.Parts.SelectMany(x => x.PartDefinition.Fields).ToList();
+                var fields = ctpd.PartDefinition.Fields.ToList();
+                string tipofield = "";
+                foreach (ContentPartFieldDefinition singleField in fields) {
+                    tipofield = singleField.FieldDefinition.Name;
+                    if (tipofield == typeof(TaxonomyField).Name) {
+
+                        #region Tassonomia in Lingua
+
+                        var taxobase = _taxonomyService.GetTaxonomyByName(singleField.Settings["TaxonomyFieldSettings.Taxonomy"]);
+                        int idmaster = taxobase.Id;
+                        if (((dynamic)taxobase.ContentItem).LocalizationPart.MasterContentItem != null) {
+                            idmaster = ((dynamic)taxobase.ContentItem).LocalizationPart.MasterContentItem.Id;
+                        }
+                        if (((dynamic)taxobase.ContentItem).LocalizationPart.Culture.Culture != Language) {
+                            taxobase = _taxonomyService.GetTaxonomies().Where(x => (x.Id == idmaster || (((dynamic)x.ContentItem).LocalizationPart.MasterContentItem != null && ((dynamic)x.ContentItem).LocalizationPart.MasterContentItem.Id == idmaster)) && ((dynamic)x.ContentItem).LocalizationPart.Culture.Culture == Language).FirstOrDefault();
+                        }
+                        List<TermPart> cata = _taxonomyService.GetTerms(taxobase.Id).ToList();//.GetTermsForContentItem(currentUser.Id, singleField.Name).ToList();
+                        List<string> ListCategory = new List<string>();
+                        ListCategory = cata.Select(x => x.Id.ToString()).ToList();
+                        List<ElementDetail> elements = new List<ElementDetail>();
+                        foreach (var term in taxobase.Terms) {
+                            Int32? valore = term.Id;
+                            Int32? mediaid = null;
+                            try {
+                                MediaLibraryPickerField mpf = (MediaLibraryPickerField)(term.Fields.Where(x => x.FieldDefinition.Name == "MediaLibraryPickerField").FirstOrDefault());
+                                mediaid = mpf.Ids[0];
+                            }
+                            catch { }
+                            if (!term.Selectable)
+                                valore = null;
+                            if (term.FullPath == "/" + term.Id)
+                                elements.Add(new ElementDetail() { Name = term.Name, Value = valore, ImageId = mediaid });
+                            else {
+                                Int32 idtermfather = Convert.ToInt32(term.FullPath.Split('/')[term.FullPath.Split('/').Length - 2]);
+                                FindTaxoVM(elements, idtermfather).Children.Add(new ElementDetail() { Name = term.Name, Value = valore, ImageId = mediaid });
+                            }
+                        }
+                        ResponseElement re = new ResponseElement();
+                        bool solofoglie = Convert.ToBoolean(singleField.Settings["TaxonomyFieldSettings.LeavesOnly"]);
+                        if (solofoglie) {
+                            ElementDetail TempElement = new ElementDetail(); //elemento fittizzio per procedura ricorsiva
+                            TempElement.Children = elements;
+                            AnnullaNonFoglie(TempElement);
+                            elements = TempElement.Children;
+                        }
+                        re.Values = elements;
+                        re.Setting = new ResponseSetting { Type = "Taxonomie", Required = Convert.ToBoolean(singleField.Settings["TaxonomyFieldSettings.Required"]), SingleChoice = Convert.ToBoolean(singleField.Settings["TaxonomyFieldSettings.SingleChoice"]) };
+                        eObj.Add(ctpd.PartDefinition.Name + "." + singleField.Name, re);
+                        #endregion Tassonomia in Lingua
+                    }
+                    else
+                        if (tipofield == typeof(EnumerationField).Name) {
+                            string[] elencovalori = singleField.Settings["EnumerationFieldSettings.Options"].Split(new string[] { "\r\n" }, StringSplitOptions.None);
+                            List<string> elencoValoriInLingua = new List<string>();
+                            List<ElementDetail> ele = new List<ElementDetail>();
+                            foreach (string val in elencovalori) {
+                                ElementDetail tvm = new ElementDetail();
+                                tvm.Name = val;
+                                tvm.Value = _localizedStringManager.GetLocalizedString("UserEnumeratore", val, Language);
+                                ele.Add(tvm);
+                            }
+                            ResponseElement re = new ResponseElement();
+                            re.Values = ele;
+                            bool singlechoise = true;
+                            if (singleField.Settings["EnumerationFieldSettings.ListMode"] == "Listbox" || singleField.Settings["EnumerationFieldSettings.ListMode"] == "Checkbox")
+                                singlechoise = false;
+                            re.Setting = new ResponseSetting { Type = "Enumerator", Required = Convert.ToBoolean(singleField.Settings["EnumerationFieldSettings.Required"]), SingleChoice = singlechoise };
+                            eObj.Add(ctpd.PartDefinition.Name + "." + singleField.Name, re);
+                        }
+                }
+            }
+
+            #endregion Tutti i field
+
+            return Json(eObj);
+        }
+
+        private void AnnullaNonFoglie(ElementDetail myelement) {
+            if (myelement.Children.Count > 0)
+                myelement.Value = null;
+            foreach (ElementDetail el in myelement.Children) {
+                AnnullaNonFoglie(el);
+            }
+        }
+
+        public class ResponseElement {
+
+            //     public object Default { get; set; }
+            public object Values { get; set; }
+
+            public ResponseSetting Setting { get; set; }
+        }
+
+        public class ResponseSetting {
+            public string Type { get; set; }
+            public bool Required { get; set; }
+            public bool SingleChoice { get; set; }
+        }
+
+        public class ElementDetail {
+
+            public ElementDetail() {
+                Name = "";
+                Children = new List<ElementDetail>();
+            }
+
+            public string Name { get; set; }
+            public object Value { get; set; }
+            public Int32? ImageId { get; set; }
+            public List<ElementDetail> Children { get; set; }
+        }
+
+        private ElementDetail FindTaxoVM(List<ElementDetail> elements, Int32 idToFind) {
+            if (elements != null) {
+                foreach (ElementDetail myterm in elements) {
+                    if ((Int32)myterm.Value == idToFind)
+                        return myterm;
+                    else
+                        return FindTaxoVM(myterm.Children, idToFind);
+                }
+                return null;
+            }
+            else
+                return null;
         }
 
         /// <summary>
@@ -114,6 +256,8 @@ namespace Laser.Orchard.ContentExtension.Controllers {
                 }
         }
 
+        #region private method
+
         private Response StoreNewContentItem(ExpandoObject eObj, ContentItem TheContentItem) {
             string tipoContent = ((dynamic)eObj).ContentType;
             ContentItem NewContent = _orchardServices.ContentManager.New(tipoContent);
@@ -123,15 +267,15 @@ namespace Laser.Orchard.ContentExtension.Controllers {
 
             // NewContent.As<TitlePart>.Title = "Creazione";
             _orchardServices.ContentManager.Create(NewContent, VersionOptions.Draft);// se non faccio il create poi non vengono salvati i field
-          Response rsp =new Response();
-            string validateMessage=ValidateMessage(NewContent,"Created");
-            if (string.IsNullOrEmpty(validateMessage)){
-               rsp = _contentExtensionsServices.StoreInspectExpando(eObj, NewContent);
-          }
-          else {
-              rsp = _utilsServices.GetResponse(ResponseType.None, validateMessage);
+            Response rsp = new Response();
+            string validateMessage = ValidateMessage(NewContent, "Created");
+            if (string.IsNullOrEmpty(validateMessage)) {
+                rsp = _contentExtensionsServices.StoreInspectExpando(eObj, NewContent);
             }
-           
+            else {
+                rsp = _utilsServices.GetResponse(ResponseType.None, validateMessage);
+            }
+
             if (rsp.Success) {
                 try {
                     string language = "";
@@ -149,10 +293,8 @@ namespace Laser.Orchard.ContentExtension.Controllers {
                         _orchardServices.ContentManager.Create(NewContent, VersionOptions.Draft);
                     }
                     else {
-                        rsp=_utilsServices.GetResponse(ResponseType.None, validateMessage);
+                        rsp = _utilsServices.GetResponse(ResponseType.None, validateMessage);
                     }
-
-                   
 
                     // _localizationService.SetContentCulture(NewContent, language);
                     if (((dynamic)NewContent).AutoroutePart != null) {
@@ -195,21 +337,20 @@ namespace Laser.Orchard.ContentExtension.Controllers {
             string validate_folder = HostingEnvironment.MapPath("~/") + @"App_Data\Sites\" + _shellSettings.Name + @"\Validation\";
             if (!System.IO.Directory.Exists(validate_folder))
                 System.IO.Directory.CreateDirectory(validate_folder);
-            string myfile = HostingEnvironment.MapPath("~/") + @"App_Data\Sites\" + _shellSettings.Name + @"\Validation\" + ci.ContentType +postfix+ ".cshtml";
+            string myfile = HostingEnvironment.MapPath("~/") + @"App_Data\Sites\" + _shellSettings.Name + @"\Validation\" + ci.ContentType + postfix + ".cshtml";
             if (System.IO.File.Exists(myfile)) {
                 string mytemplate = File.ReadAllText(myfile);
                 if (!string.IsNullOrEmpty(mytemplate)) {
                     var config = new TemplateServiceConfiguration();
                     string result = "";
                     using (var service = RazorEngineService.Create(config)) {
-                        result = service.RunCompile(mytemplate, "htmlRawTemplatea",null, (Object)ci);
+                        result = service.RunCompile(mytemplate, "htmlRawTemplatea", null, (Object)ci);
                     }
                     string resultnobr = result.Replace("\r\n", "").Replace(" ", "");
-                   if (!string.IsNullOrEmpty(resultnobr)) {
+                    if (!string.IsNullOrEmpty(resultnobr)) {
                         return result;
                     }
                 }
-
             }
             return null;
         }
@@ -223,8 +364,12 @@ namespace Laser.Orchard.ContentExtension.Controllers {
             //else
             return true;
         }
+
+        #endregion private method
     }
-    class MyIReferenceResolver : IReferenceResolver {
+
+    internal class MyIReferenceResolver : IReferenceResolver {
+
         //public string FindLoaded(IEnumerable<string> refs, string find) {
         //    return refs.First(r => r.EndsWith(System.IO.Path.DirectorySeparatorChar + find));
         //}
@@ -236,7 +381,7 @@ namespace Laser.Orchard.ContentExtension.Controllers {
             // TypeContext gives you some context for the compilation (which templates, which namespaces and types)
 
             // You must make sure to include all libraries that are required!
-            // Mono compiler does add more standard references than csc! 
+            // Mono compiler does add more standard references than csc!
             // If you want mono compatibility include ALL references here, including mscorlib!
             // If you include mscorlib here the compiler is called with /nostdlib.
             //IEnumerable<string> loadedAssemblies = (new UseCurrentAssembliesReferenceResolver())
@@ -244,10 +389,10 @@ namespace Laser.Orchard.ContentExtension.Controllers {
             //    .Select(r => r.GetFile())
             //    .ToArray();
 
-        //    yield return CompilerReference.From(FindLoaded(loadedAssemblies, "mscorlib.dll"));
-        //    yield return CompilerReference.From(FindLoaded(loadedAssemblies, "System.dll"));
-      //      yield return CompilerReference.From(FindLoaded(loadedAssemblies, "System.Core.dll"));
-       //     yield return CompilerReference.From(typeof(MyIReferenceResolver).Assembly); // Assembly
+            //    yield return CompilerReference.From(FindLoaded(loadedAssemblies, "mscorlib.dll"));
+            //    yield return CompilerReference.From(FindLoaded(loadedAssemblies, "System.dll"));
+            //      yield return CompilerReference.From(FindLoaded(loadedAssemblies, "System.Core.dll"));
+            //     yield return CompilerReference.From(typeof(MyIReferenceResolver).Assembly); // Assembly
 
             // There are several ways to load an assembly:
             //yield return CompilerReference.From("Path-to-my-custom-assembly"); // file path (string)
