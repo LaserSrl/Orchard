15909962910f6b892ad2b1d4a28a6c73b4d27548
Lorenzo <lorenzo.frediani@laser-group.com>
2016-02-23 10:59:11 +0100

    add permission

--- src/Orchard.Web/Modules/Orchard.JobsQueue/AdminMenu.cs
+++ src/Orchard.Web/Modules/Orchard.JobsQueue/AdminMenu.cs
@@ -1,4 +1,5 @@
 ﻿using Orchard.Environment.Extensions;
+using Orchard.Security;
 using Orchard.UI.Navigation;
 
 namespace Orchard.JobsQueue {
@@ -10,9 +11,12 @@ namespace Orchard.JobsQueue {
         public void GetNavigation(NavigationBuilder builder) {
             builder
                 .AddImageSet("jobsqueue")
-                .Add(T("Jobs Queue"), "15.0", item => {
+                .Add(T("Jobs Queue"), "15.0", item
+                    => {
+                    item.Permission(StandardPermissions.SiteOwner);
                     item.Action("List", "Admin", new { area = "Orchard.JobsQueue" });
                     item.LinkToFirstChild(false);
+                   
                 });
         }
     }
