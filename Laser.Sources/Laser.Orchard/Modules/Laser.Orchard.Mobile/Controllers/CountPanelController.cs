94d9121c709ec69a8e25317db9387ad475a9bc97
giuseppe.musso <giuseppe.musso@laser-group.com>
2016-03-01 15:23:35 +0100

    Fix: aggiunti nuovi file su Git.

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.Mobile/Controllers/CountPanelController.cs
@@ -0,0 +1,33 @@
+﻿using Laser.Orchard.Mobile.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laser.Orchard.Mobile.Controllers
+{
+    public class CountPanelController : Controller
+    {
+        private readonly IPushNotificationService _pushNotificationService;
+        public CountPanelController(IPushNotificationService pushNotificationService)
+        {
+            _pushNotificationService = pushNotificationService;
+        }
+        [HttpGet]
+        public JsonResult GetTotal(Int32[] ids, Int32? idlocalization, Int32? tot)
+        {
+            Dictionary<string, string> Total = new Dictionary<string, string>();
+            Total.Add("Key", "<i class=\"fa fa-mobile\"></i>");
+            if (tot.HasValue)
+            {
+                Total.Add("Value", tot.ToString());
+            }
+            else
+            {
+                Total.Add("Value", _pushNotificationService.GetPushQueryResult(ids).Count.ToString());
+            }
+            return Json(Total, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
