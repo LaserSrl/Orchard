aa497296d5568e98ce20045c6d9ccf91612e911e
Lorenzo <lorenzo.frediani@laser-group.com>
2016-03-14 14:44:27 +0100

    gestione cache e log

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.DevTools/ViewModels/Segnalazione.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laser.Orchard.DevTools.ViewModels {
+    public class Segnalazione {
+       public string Testo { get; set; }
+    }
+}
