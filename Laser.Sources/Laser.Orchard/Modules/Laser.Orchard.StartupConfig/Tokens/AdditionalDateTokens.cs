06f9d8f378c2178ce726999da8523cda3b553413
HermesSbicego-Laser <hermes.sbicego@laser-group.com>
2016-04-05 14:50:07 +0200

    - fix, token Date.Add:*(n)

--- Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.StartupConfig/Tokens/AdditionalDateTokens.cs
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.StartupConfig/Tokens/AdditionalDateTokens.cs
@@ -5,14 +5,18 @@ using Orchard.Localization;
 using Orchard.Tokens;
 using System.Globalization;
 using Orchard;
+using Orchard.Services;
 
 namespace Laser.Orchard.StartupConfig.Tokens {
     public class AdditionalDateTokens : ITokenProvider {
         private readonly IWorkContextAccessor _workContextAccessor;
+        private readonly IClock _clock;
+
         private DateTime _resultDate;
         private string _fullTokenName;
-        public AdditionalDateTokens(IWorkContextAccessor workContextAccessor) {
+        public AdditionalDateTokens(IWorkContextAccessor workContextAccessor, IClock clock) {
             _workContextAccessor = workContextAccessor;
+            _clock = clock;
 
             T = NullLocalizer.Instance;
         }
@@ -26,27 +30,33 @@ namespace Laser.Orchard.StartupConfig.Tokens {
         }
 
         public void Evaluate(EvaluateContext context) {
-            context.For<DateTime>("Date")
+            context.For<DateTime>("Date", () => _clock.UtcNow)
                 .Token(
                 token => token.StartsWith("Add:", StringComparison.OrdinalIgnoreCase) ? token.Substring(0, (token.IndexOf(".") > 0 ? token.IndexOf(".") : token.Length)) : "",
                     (token, d) => {
-                        if (token == "") return null;
                         _fullTokenName = token;
-                        var parenthesysPosition = token.IndexOf("(");
-                        var function = token.Substring(0, parenthesysPosition);
-                        var number = token.Substring(parenthesysPosition + 1, token.Length - parenthesysPosition - 2);
-                        if (function.Equals("Add:Days", StringComparison.InvariantCultureIgnoreCase)) {
-                            return d.AddDays(Convert.ToDouble(number));
-                        } else if (function.Equals("Add:Months", StringComparison.InvariantCultureIgnoreCase)) {
-                            return d.AddMonths(Convert.ToInt32(number));
-                        } else if (function.Equals("Add:Years", StringComparison.InvariantCultureIgnoreCase)) {
-                            return d.AddYears(Convert.ToInt32(number));
-                        } else {
-                            return null;
-                        }
+                        return GetDatebyToken(token, d);
                     }
                 )
-                .Chain(_fullTokenName, "Date", (d) => d); // il nome del token passato alla chain deve essere identico al token presente nel primo parametro del Token(tokeName,...,...)
+                .Chain(_fullTokenName, "Date", (d) => GetDatebyToken(_fullTokenName, d)); // il nome del token passato alla chain deve essere identico al token presente nel primo parametro del Token(tokeName,...,...)
+        }
+
+        private DateTime? GetDatebyToken(string token, DateTime originalDate) {
+            if (token == "") return null;
+            var parenthesysPosition = token.IndexOf("(");
+            var function = token.Substring(0, parenthesysPosition);
+            var number = token.Substring(parenthesysPosition + 1, token.Length - parenthesysPosition - 2);
+            if (function.Equals("Add:Days", StringComparison.InvariantCultureIgnoreCase)) {
+                return originalDate.AddDays(Convert.ToDouble(number));
+            } else if (function.Equals("Add:Months", StringComparison.InvariantCultureIgnoreCase)) {
+                return originalDate.AddMonths(Convert.ToInt32(number));
+            } else if (function.Equals("Add:Years", StringComparison.InvariantCultureIgnoreCase)) {
+                return originalDate.AddYears(Convert.ToInt32(number));
+            } else {
+                return null;
+            }
+        
+        
         }
     }
 }
