5a014055a41b1e107265424697b6a8e408b2467a
giuseppe.musso <giuseppe.musso@laser-group.com>
2016-04-06 12:05:30 +0200

    Communication Gateway Contacts: ricerca anche per mail e telefono.

--- Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.CommunicationGateway/ViewModels/SearchVM.cs
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.CommunicationGateway/ViewModels/SearchVM.cs
@@ -1,6 +1,12 @@
-﻿namespace Laser.Orchard.CommunicationGateway {
+﻿using Laser.Orchard.CommunicationGateway.ViewModels;
+namespace Laser.Orchard.CommunicationGateway {
 
     public class SearchVM {
         public string Expression { get; set; }
+        public SearchFieldEnum Field { get; set; }
+
+        public SearchVM() {
+            Field = SearchFieldEnum.Name; // default value
+        }
     }
 }
