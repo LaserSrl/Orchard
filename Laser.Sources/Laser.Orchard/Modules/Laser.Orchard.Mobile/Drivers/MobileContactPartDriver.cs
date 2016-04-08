d1c2027464106ebc71a120baaffe77004be1430c
Lorenzo <lorenzo.frediani@laser-group.com>
2016-04-08 14:31:37 +0200

    fix su lista vuota

--- Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.Mobile/Drivers/MobileContactPartDriver.cs
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.Mobile/Drivers/MobileContactPartDriver.cs
@@ -1,27 +1,21 @@
-﻿using Laser.Orchard.CommunicationGateway.Models;
-using Laser.Orchard.Mobile.Models;
+﻿using Laser.Orchard.Mobile.Models;
 using Orchard.ContentManagement.Drivers;
-using Orchard.Data;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 
 namespace Laser.Orchard.Mobile.Drivers {
+
     public class MobileContactPartDriver : ContentPartDriver<MobileContactPart> {
-     //   private readonly IRepository<PushNotificationRecord> _repositoryPushNotificationRecord;
+
         protected override string Prefix {
             get { return "Laser.Mobile.MobileContact"; }
         }
 
-        //public MobileContactPartDriver(IRepository<PushNotificationRecord> repositoryPushNotificationRecord) {
-        //    _repositoryPushNotificationRecord =  repositoryPushNotificationRecord;
-        //}
-
         protected override DriverResult Editor(MobileContactPart part, dynamic shapeHelper) {
-            List<PushNotificationRecord> viewModel = part.MobileEntries.Value.ToList();
+            List<PushNotificationRecord> viewModel = new List<PushNotificationRecord>();
+            if (part.MobileEntries != null && part.MobileEntries.Value != null && part.MobileEntries.Value.Count > 0)
+                viewModel = part.MobileEntries.Value.ToList();
             return ContentShape("Parts_MobileContact_Edit", () => shapeHelper.EditorTemplate(TemplateName: "Parts/MobileContact_Edit", Model: viewModel, Prefix: Prefix));
-         }
+        }
     }
-
 }
