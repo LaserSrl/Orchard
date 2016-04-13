1de3d2f1936452469f31695b13e036765df13cb7
marco.viglione <marco.viglione@laser-group.com>
2016-04-13 12:54:38 +0200

    Modificati nome e contesto token. Modificati token in modo che si applichino in automatico al current content item. Aggiunta possibilità di usare token in chain.

--- Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.Events/Tokens/EventTokens.cs
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.Events/Tokens/EventTokens.cs
@@ -1,5 +1,7 @@
 ﻿using Laser.Orchard.Events.Models;
+using Laser.Orchard.StartupConfig.Services;
 using Orchard;
+using Orchard.ContentManagement;
 using Orchard.Events;
 using Orchard.Localization;
 using Orchard.Tokens;
@@ -17,14 +19,16 @@ namespace Laser.Orchard.Events.Tokens
 
     public class EventTokens : ITokenProvider
     {
+        private readonly ICurrentContentAccessor _currentContentAccessor;
         private readonly ITokenizer _tokenizer;
         private readonly IWorkContextAccessor _workContextAccessor;
         private readonly Lazy<CultureInfo> _cultureInfo;
 
         public Localizer T { get; set; }
 
-        public EventTokens(ITokenizer tokenizer, IWorkContextAccessor workContextAccessor)
+        public EventTokens(ICurrentContentAccessor currentContentAccessor, ITokenizer tokenizer, IWorkContextAccessor workContextAccessor)
         {
+            _currentContentAccessor = currentContentAccessor;
             _tokenizer = tokenizer;
             _workContextAccessor = workContextAccessor;
             _cultureInfo = new Lazy<CultureInfo>(() => CultureInfo.GetCultureInfo(_workContextAccessor.GetContext().CurrentSite.SiteCulture));
@@ -33,39 +37,45 @@ namespace Laser.Orchard.Events.Tokens
 
         public void Describe(DescribeContext context)
         {
-            context.For("EventList", T("Event List"), T("Tokens for Calendars (list shape)"))
-                   .Token("StartDate", T("List Start Date"), T("Start date of the parent Calendar"))
-                   .Token("EndDate", T("List End Date"), T("End date of the parent Calendar"));
+            context.For("Content", T("Event List"), T("Tokens for Calendars (list shape)"))
+                   .Token("CalendarStartDate", T("List Start Date"), T("Start date of the parent Calendar"))
+                   .Token("CalendarEndDate", T("List End Date"), T("End date of the parent Calendar"));
         }
 
         public void Evaluate(EvaluateContext context)
         {
-            context.For<CalendarPart>("EventList")
-                .Token("StartDate", GetStartDate)
-                .Token("EndDate", GetEndDate);
+            context.For("Content", _currentContentAccessor.CurrentContentItem)
+                   .Token("CalendarStartDate", content => GetStartDate(content))
+                   .Chain("CalendarStartDate", "Date", content => { DateTime dateResult; if (DateTime.TryParse(GetStartDate(content), _cultureInfo.Value, DateTimeStyles.None, out dateResult)) return dateResult; else return null; })
+                   .Token("CalendarEndDate", content => GetEndDate(content))
+                   .Chain("CalendarEndDate", "Date", content => { DateTime dateResult; if (DateTime.TryParse(GetEndDate(content), _cultureInfo.Value, DateTimeStyles.None, out dateResult)) return dateResult; else return null; });
         }
 
-
-        private string GetStartDate(CalendarPart part)
+        private string GetStartDate(ContentItem content)
         {
-            if (part == null)
+            CalendarPart calendar = content == null ? null : content.As<CalendarPart>();
+
+            if (calendar == null)
                 return null;
             else
-                return _tokenizer.Replace(part.StartDate, new Dictionary<string, object>());
+                return _tokenizer.Replace(calendar.StartDate, new Dictionary<string, object>());
         }
 
-        private string GetEndDate(CalendarPart part)
+        private string GetEndDate(ContentItem content)
         {
-            if (part == null) return null;
+            CalendarPart calendar = content == null ? null : content.As<CalendarPart>();
 
-            DateTime startDate = Convert.ToDateTime(_tokenizer.Replace(part.StartDate, new Dictionary<string, object>()), _cultureInfo.Value);
+            if (calendar == null)
+                return null;
+            else {
+                DateTime startDate = Convert.ToDateTime(_tokenizer.Replace(calendar.StartDate, new Dictionary<string, object>()), _cultureInfo.Value);
 
-            int duration = 1; //Valore standard da usare se la conversione fallisce
-            int.TryParse(_tokenizer.Replace(part.NumDays, new Dictionary<string, object>()), out duration);
-            if (duration <= 0) duration = 1;
+                int duration = 1; //Valore standard da usare se la conversione fallisce
+                int.TryParse(_tokenizer.Replace(calendar.NumDays, new Dictionary<string, object>()), out duration);
+                if (duration <= 0) duration = 1;
 
-            return startDate.AddDays(duration - 1).ToString(_cultureInfo.Value);
+                return startDate.AddDays(duration - 1).ToString(_cultureInfo.Value);
+            }
         }
-
     }
 }
