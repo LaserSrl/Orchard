2fbb2a525a9247ac9960d43f8868fcc482ef9a86
giuseppe.musso <giuseppe.musso@laser-group.com>
2016-05-18 11:45:36 +0200

    Sincronizzazione contatti: aggiunto controllo per la costruzione del Title. Open Authentication: visualizzazione del provider nel dettaglio degli utenti.

--- Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.CommunicationGateway/Services/ICommunicationService.cs
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.CommunicationGateway/Services/ICommunicationService.cs
@@ -380,7 +380,11 @@ namespace Laser.Orchard.CommunicationGateway.Services {
                     _repositoryCommunicationEmailRecord.Flush();
                 }
             }
-            Contact.As<TitlePart>().Title = UserContent.Email + " " + UserContent.UserName;
+            if (UserContent.Email == UserContent.UserName) {
+                Contact.As<TitlePart>().Title = UserContent.UserName;
+            } else {
+                Contact.As<TitlePart>().Title = UserContent.Email + " " + UserContent.UserName;
+            }
             dynamic mypart = (((dynamic)Contact).CommunicationContactPart);
             mypart.GetType().GetProperty("UserIdentifier").SetValue(mypart, UserContent.Id, null);
             if (asProfilePart) {
