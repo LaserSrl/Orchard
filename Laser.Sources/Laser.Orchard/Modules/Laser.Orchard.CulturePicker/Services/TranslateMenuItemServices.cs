1f22fb2bc5d1dfc2e037f88e0156b3cd0873e4f8
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2016-05-26 15:19:38 +0200

    Al momento la "traduzione" di un menu crea un menu duplicato con la stessa struttura di menu items, ma senza riempire contenuti dei menu items.

--- Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.CulturePicker/Services/TranslateMenuItemServices.cs
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.CulturePicker/Services/TranslateMenuItemServices.cs
@@ -9,6 +9,9 @@ using Orchard.Localization.Models;
 using System.Linq;
 using Orchard.UI.Navigation;
 using Orchard.Core.Navigation.Models;
+using Orchard.Data;
+using NHibernate.Criterion;
+using Orchard.Core.Navigation.Services;
 
 namespace Laser.Orchard.CulturePicker.Services {
     [OrchardFeature("Laser.Orchard.CulturePicker.TranslateMenuItems")]
@@ -16,16 +19,24 @@ namespace Laser.Orchard.CulturePicker.Services {
 
         private readonly IContentManager _contentManager;
         private readonly ILocalizationService _localizationService;
+        private readonly ISessionLocator _sessionLocator;
+        private readonly IMenuService _menuService;
 
         public TranslateMenuItemServices(
             ILocalizationService localizationService, 
-            IContentManager contentManager) {
+            IContentManager contentManager,
+            ISessionLocator sessionLocator,
+            IMenuService menuService) {
 
             _localizationService = localizationService;
             _contentManager = contentManager;
+            _sessionLocator = sessionLocator;
+            _menuService = menuService;
         }
         public bool TryTranslateAllSubmenus(TranslateMenuItemsPart part) {
             var menu = part.ContentItem;
+            //get the Id of the translated menu
+            int tMenuId = menu.Id;
             //get the target culture from the localization part in the menu
             var locPart = menu.As<LocalizationPart>(); //this contains the localization info
             string targetCulture = locPart.Culture.Culture;
@@ -39,9 +50,26 @@ namespace Laser.Orchard.CulturePicker.Services {
                     break;
                 }
             }
-            var masterMenuItem = _contentManager.Get(masterId).As<MenuPart>();
+            var masterMenu = _menuService.GetMenu(masterId);//_contentManager.Get(masterId);
+            var thisMenu = _menuService.GetMenu(tMenuId);
 
-            return false;
+            var masterParts = _menuService.GetMenuParts(masterId);
+
+            //duplicate the parts into the new menu
+            foreach (var origPart in masterParts) {
+                var menuPart = _contentManager.New<MenuPart>(origPart.ContentItem.ContentType);
+                menuPart.MenuPosition = origPart.MenuPosition;
+                menuPart.Menu = thisMenu;
+                menuPart.MenuText = origPart.MenuText + " " + targetCulture;
+                //Up to this point, we have replicated the menu structure (for this item)
+                //We should see to relocalize the content of the menu item IF it has a translation already
+
+                _contentManager.Create(menuPart);
+
+            }
+
+            return true;
         }
+
     }
 }
