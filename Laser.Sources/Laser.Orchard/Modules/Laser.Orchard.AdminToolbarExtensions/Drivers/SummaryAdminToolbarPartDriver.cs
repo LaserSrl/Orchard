038c322c7effff09fe6f6f9c799a53f68b51218e
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2016-06-10 09:19:58 +0200

    Added migrations, settings, other required files for the new module Working on editor for toolbar extension at the type creation window, using part settings

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.AdminToolbarExtensions/Drivers/SummaryAdminToolbarPartDriver.cs
@@ -0,0 +1,24 @@
+﻿using Laser.Orchard.AdminToolbarExtensions.Models;
+using Orchard.ContentManagement.Drivers;
+using Orchard.Environment.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laser.Orchard.AdminToolbarExtensions.Drivers {
+    [OrchardFeature("Laser.Orchard.AdminToolbarExtensions.SummaryAdminToolbar")]
+    public class SummaryAdminToolbarPartDriver : ContentPartDriver<SummaryAdminToolbarPart> {
+
+
+        protected override DriverResult Display(SummaryAdminToolbarPart part, string displayType, dynamic shapeHelper) {
+
+            var barSettings = part.Settings.GetModel<SummaryAdminToolbarSettings>();
+            if (displayType == "SummaryAdmin") {
+                return ContentShape("Parts_SummaryAdminToolbarPart_SummaryAdmin",
+                    () => shapeHelper.Parts_SummaryAdminToolbarPart_SummaryAdmin(Toolbar: barSettings));
+            }
+            return new DriverResult();//base.Display(part, displayType, shapeHelper);
+        }
+    }
+}
