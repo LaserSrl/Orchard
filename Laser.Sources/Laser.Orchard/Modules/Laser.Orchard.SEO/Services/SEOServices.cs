7365cac847a48128eeea10f04d400ca6e221f0ee
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2016-06-21 17:06:17 +0200

    gestion orari UTC locale rimosso riga di debug

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.SEO/Services/SEOServices.cs
@@ -0,0 +1,34 @@
+﻿using Laser.Orchard.StartupConfig.Localization;
+using Orchard;
+using Orchard.Localization.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laser.Orchard.SEO.Services {
+    public interface ISEOServices : IDependency {
+        DateTime DateToLocal(DateTime utcDate);
+        DateTime DateToUTC(DateTime localDate);
+    }
+
+    public class SEOServices : ISEOServices{
+
+        private readonly IDateServices _dateServices;
+        private readonly IDateLocalization _dateLocalization;
+
+        public SEOServices(IDateServices dateServices, IDateLocalization dateLocalization){
+            _dateServices = dateServices;
+            _dateLocalization = dateLocalization;
+        }
+
+        public DateTime DateToLocal(DateTime utcDate) {
+            return (DateTime)_dateServices.ConvertToLocal(utcDate);
+        }
+
+        public DateTime DateToUTC(DateTime localDate) {
+            return (DateTime)(_dateServices.ConvertFromLocalString(_dateLocalization.WriteDateLocalized(localDate), _dateLocalization.WriteTimeLocalized(localDate)));
+            
+        }
+    }
+}
---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.SEO/ViewModels/SeoPartViewModel.cs
@@ -0,0 +1,60 @@
+﻿using Laser.Orchard.SEO.Models;
+using Laser.Orchard.SEO.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laser.Orchard.SEO.ViewModels {
+    public class SeoPartViewModel {
+        public string TitleOverride { get; set; }
+        public string Keywords { get; set; }
+        public string Description { get; set; }
+        public bool RobotsNoIndex { get; set; }
+        public bool RobotsNoFollow { get; set; }
+        public bool RobotsNoSnippet { get; set; }
+        public bool RobotsNoOdp { get; set; }
+        public bool RobotsNoArchive { get; set; }
+        public bool RobotsUnavailableAfter { get; set; }
+        public DateTime RobotsUnavailableAfterDate { get; set; }
+        public bool RobotsNoImageIndex { get; set; }
+        public bool GoogleNoSiteLinkSearchBox { get; set; }
+        public bool GoogleNoTranslate { get; set; }
+
+        public SeoPartViewModel() {
+        }
+
+        public SeoPartViewModel(SeoPart part, ISEOServices seoServices) {
+
+            this.TitleOverride = part.TitleOverride;
+            this.Keywords = part.Keywords;
+            this.Description = part.Description;
+            this.RobotsNoIndex = part.RobotsNoIndex;
+            this.RobotsNoFollow = part.RobotsNoFollow;
+            this.RobotsNoSnippet = part.RobotsNoSnippet;
+            this.RobotsNoOdp = part.RobotsNoOdp;
+            this.RobotsNoArchive = part.RobotsNoArchive;
+            this.RobotsUnavailableAfter = part.RobotsUnavailableAfter;
+            this.RobotsUnavailableAfterDate = seoServices.DateToLocal(part.RobotsUnavailableAfterDate);
+            this.RobotsNoImageIndex = part.RobotsNoImageIndex;
+            this.GoogleNoSiteLinkSearchBox = part.GoogleNoSiteLinkSearchBox;
+            this.GoogleNoTranslate = part.GoogleNoTranslate;
+        }
+
+        public void UpdatePart(SeoPart part, ISEOServices seoServices) {
+            part.TitleOverride = this.TitleOverride;
+            part.Keywords = this.Keywords;
+            part.Description = this.Description;
+            part.RobotsNoIndex = this.RobotsNoIndex;
+            part.RobotsNoFollow = this.RobotsNoFollow;
+            part.RobotsNoSnippet = this.RobotsNoSnippet;
+            part.RobotsNoOdp = this.RobotsNoOdp;
+            part.RobotsNoArchive = this.RobotsNoArchive;
+            part.RobotsUnavailableAfter = this.RobotsUnavailableAfter;
+            part.RobotsUnavailableAfterDate = seoServices.DateToUTC(this.RobotsUnavailableAfterDate);
+            part.RobotsNoImageIndex = this.RobotsNoImageIndex;
+            part.GoogleNoSiteLinkSearchBox = this.GoogleNoSiteLinkSearchBox;
+            part.GoogleNoTranslate = this.GoogleNoTranslate;
+        }
+    }
+}
