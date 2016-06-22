c780a120805b6f09488cd202cd954b12ed36d5a4
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2016-06-22 14:20:55 +0200

    Aggiunto nuovo modulo per la creazione di un Field Hidden. Tramite logiche di permessi, solo alcuni ruoli possono vedere o modificare i contenuti di questo field. Rimane da gestire la tokenizzazione del campo.

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.HiddenFields/Settings/HiddenStringFieldSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laser.Orchard.HiddenFields.Settings {
+    public class HiddenStringFieldSettings {
+        public bool Required { get; set; }
+        public bool Tokenized { get; set; } //The token selection is always active. However, token substitution is only active when this is true.
+        public string TemplateString { get; set; }
+
+        public HiddenStringFieldSettings() {
+            Tokenized = true;
+        }
+    }
+}
