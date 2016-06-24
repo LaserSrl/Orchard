cd5b22b484d5ec22c6ef543d119f632bd68f8dad
HermesSbicego-Laser <hermes.sbicego@laser-group.com>
2016-06-24 16:16:48 +0200

    - ApiController per reactions

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.UserReactions/Controllers/ReactionApiController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using Laser.Orchard.UserReactions.Services;
+using Laser.Orchard.UserReactions.ViewModels;
+
+namespace Laser.Orchard.UserReactions.Controllers {
+    public class ReactionApiController : ApiController {
+
+        private readonly IUserReactionsService _userReactionService;
+
+        public ReactionApiController(IUserReactionsService userReactionService) {
+            _userReactionService = userReactionService;
+        }
+
+        public UserReactionsVM[] Get(int pageId) {
+            UserReactionsVM[] typeClick = null;
+            typeClick = _userReactionService.GetSummaryReaction(pageId);
+            return typeClick;
+
+        }
+        public UserReactionsVM Post(int reactionTypeId, int pageId) {
+            UserReactionsVM typeClick = null;
+            typeClick = _userReactionService.CalculateTypeClick(reactionTypeId, pageId);
+            return typeClick;
+        }
+
+    }
+}
