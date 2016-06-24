d22392a40879eb037edcbaf1ce3e6028279f5950
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2016-06-24 17:23:18 +0200

    Primo prototipo implementativo della feature per i keyword tools usando google trends

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.SEO/Drivers/KeywordHelperDriver.cs
@@ -0,0 +1,41 @@
+﻿using Laser.Orchard.SEO.Models;
+using Laser.Orchard.SEO.ViewModels;
+using Orchard.ContentManagement;
+using Orchard.ContentManagement.Drivers;
+using Orchard.Environment.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laser.Orchard.SEO.Drivers {
+    [OrchardFeature("Laser.Orchard.KeywordHelper")]
+    public class KeywordHelperDriver : ContentPartDriver<KeywordHelperPart> {
+
+        public KeywordHelperDriver() {
+
+        }
+
+        /// <summary>
+        /// GET Editor
+        /// </summary>
+        protected override DriverResult Editor(KeywordHelperPart part, dynamic shapeHelper) {
+            return ContentShape("Parts_KeywordHelper_Edit",
+                () => shapeHelper.EditorTemplate(
+                    TemplateName: "Parts/KeywordHelper",
+                    Model: new KeywordHelperPartViewModel(part),
+                    Prefix: Prefix));
+        }
+
+        /// <summary>
+        /// POST editor
+        /// </summary>
+        protected override DriverResult Editor(KeywordHelperPart part, IUpdateModel updater, dynamic shapeHelper) {
+            var vm = new KeywordHelperPartViewModel();
+            if (updater.TryUpdateModel(vm, Prefix, null, null)) {
+                part.Keywords = vm.ListToString();
+            }
+            return Editor(part,shapeHelper);
+        }
+    }
+}
--- Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.SEO/Models/KeywordHelperPart.cs
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.SEO/Models/KeywordHelperPart.cs
@@ -9,6 +9,8 @@ namespace Laser.Orchard.SEO.Models {
     [OrchardFeature("Laser.Orchard.KeywordHelper")]
     public class KeywordHelperPart : ContentPart<KeywordHelperPartVersionRecord> {
         //this is a single string that will contain a comma separated list of keywords
+        //When we build it from user input, we make sure that there are no leading or trailing white space.
+        //we also make sure that multiple white spaces in a given keyword are removed
         public string Keywords {
             get { return this.Retrieve(x => x.Keywords); }
             set { this.Store(x => x.Keywords, value); }
