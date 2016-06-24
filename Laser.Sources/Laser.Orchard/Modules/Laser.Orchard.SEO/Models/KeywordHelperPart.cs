c6e9da382e6ba3536e5b3ca9982bb46ebdb914ab
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2016-06-24 15:20:51 +0200

    iniziato lavoro su keyword tool

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.SEO/Models/KeywordHelperPart.cs
@@ -0,0 +1,17 @@
+﻿using Orchard.ContentManagement;
+using Orchard.Environment.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laser.Orchard.SEO.Models {
+    [OrchardFeature("Laser.Orchard.KeywordHelper")]
+    public class KeywordHelperPart : ContentPart<KeywordHelperPartVersionRecord> {
+        //this is a single string that will contain a comma separated list of keywords
+        public string Keywords {
+            get { return this.Retrieve(x => x.Keywords); }
+            set { this.Store(x => x.Keywords, value); }
+        }
+    }
+}
