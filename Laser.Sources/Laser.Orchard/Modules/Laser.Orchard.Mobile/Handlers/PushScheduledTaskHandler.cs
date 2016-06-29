2a179fdcff7c16f9e26e2c230d48ecf8ceebc6c4
giuseppe.musso <giuseppe.musso@laser-group.com>
2016-06-29 10:08:37 +0200

    Errori Push VisitMilano: incluso task handler e un nuovo ViewModel per l'invio delle push.

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.Mobile/Handlers/PushScheduledTaskHandler.cs
@@ -0,0 +1,37 @@
+﻿using Laser.Orchard.Mobile.Services;
+using Orchard.Logging;
+using Orchard.Tasks.Scheduling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laser.Orchard.Mobile.Handlers {
+    public class PushScheduledTaskHandler : IScheduledTaskHandler {
+        private readonly IPushNotificationService _pushNotificationService;
+        private const string TaskType = "Laser.Orchard.PushNotification.Task";
+
+        public ILogger Logger { get; set; }
+
+        public PushScheduledTaskHandler(IPushNotificationService pushNotificationService) {
+            _pushNotificationService = pushNotificationService;
+            Logger = NullLogger.Instance;
+        }
+
+        public void Process(ScheduledTaskContext context) {
+            try {
+                if (context.Task.TaskType != TaskType) {
+                    return;
+                }
+                // esegue l'invio in un thread asincrono
+                System.Threading.Tasks.Task.Run(() => {
+                    _pushNotificationService.PublishedPushEvent(context.Task.ContentItem);
+                });
+                //_pushNotificationService.PublishedPushEvent(context.Task.ContentItem);
+            }
+            catch (Exception ex) {
+                Logger.Error(ex, "Error in PushScheduledTaskHandler. ContentItem: {0}, ScheduledUtc: {1:yyyy-MM-dd HH.mm.ss} Please verify if it is necessary sending your push again.", context.Task.ContentItem, context.Task.ScheduledUtc);
+            }
+        }
+    }
+}
