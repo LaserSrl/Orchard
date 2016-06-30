ce9035e53fc18c352f220a6ad7379f67ff966c56
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2016-06-30 17:32:16 +0200

    Iniziato ad impostare discorso per l'upload vimeo

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.Vimeo/Controllers/VimeoUploadController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laser.Orchard.Vimeo.Controllers {
+    public class VimeoUploadController : Controller {
+
+        public ActionResult TryStartUpload(int fileSize) {
+            //Check the available quota against the file size.
+            //The Client calling this action should send it the filesize in bytes.
+            //The action should check the UploadsInProgress (could this be from System.Collections.Concurrent? Like a ConcurrentBag)
+            //Should UploadsInProgress be on the db?
+            //If there is not enough upload quota available, return an error or something.
+
+            return null; //just here to avoid compilation errors
+        }
+    }
+}
