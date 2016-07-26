c1bedfca39165b201fa61b8d56980e5cbd335233
giuseppe.musso <giuseppe.musso@laser-group.com>
2016-07-26 12:16:11 +0200

    TestPushInProd: inserita la funzionalità anche nelle push senza cruscotto". Aggiunta la dipendenzza Orchard in CommunicationGateway da Mobile.

--- Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.Mobile/Handlers/MobilePushPartHandler.cs
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.Mobile/Handlers/MobilePushPartHandler.cs
@@ -4,21 +4,25 @@ using Orchard;
 using Orchard.ContentManagement.Handlers;
 using Orchard.Data;
 using Orchard.Environment.Extensions;
+using Orchard.Localization;
 using Orchard.Logging;
 using Orchard.Tasks.Scheduling;
+using Orchard.UI.Notify;
 using System;
 
 namespace Laser.Orchard.Mobile.Handlers   {
     [OrchardFeature("Laser.Orchard.PushGateway")]
     public class MobilePushPartHandler : ContentHandler {
-        private readonly IPushNotificationService _pushNotificationService;
+        private readonly INotifier _notifier;
         private readonly IScheduledTaskManager _taskManager;
         private readonly IOrchardServices _orchardServices;
         private readonly IPushGatewayService _pushGatewayService;
+        public Localizer T { get; set; }
 
-        public MobilePushPartHandler(IRepository<MobilePushPartRecord> repository, IPushNotificationService pushNotificationService, IScheduledTaskManager taskManager, IOrchardServices orchardServices, IPushGatewayService pushGatewayService) {
+        public MobilePushPartHandler(IRepository<MobilePushPartRecord> repository, INotifier notifier, IScheduledTaskManager taskManager, IOrchardServices orchardServices, IPushGatewayService pushGatewayService) {
             Logger = NullLogger.Instance;
-            _pushNotificationService=pushNotificationService;
+            T = NullLocalizer.Instance;
+            _notifier = notifier;
             _orchardServices = orchardServices;
             _pushGatewayService = pushGatewayService;
             _taskManager = taskManager;
@@ -30,7 +34,7 @@ namespace Laser.Orchard.Mobile.Handlers   {
                     _pushGatewayService.PublishedPushEventTest(part.ContentItem); 
                 }
 
-                if (_orchardServices.WorkContext.HttpContext.Request.Form["submit-PushContact"] != null) {
+                if (_orchardServices.WorkContext.HttpContext.Request.Form["submit.Save"] == "submit.PushContact") {
                     // invia la push al contact selezionato
                     string contactTitle = "";
                     string aux = _orchardServices.WorkContext.HttpContext.Request.Form["contact-to-push"];
@@ -40,7 +44,10 @@ namespace Laser.Orchard.Mobile.Handlers   {
                         contactTitle = aux.Substring(0, idx);
                     }
                     // invia la push
-                    _pushGatewayService.SendPushToContact(part.ContentItem, contactTitle);
+                    if ((string.IsNullOrWhiteSpace(contactTitle) == false) && (string.IsNullOrWhiteSpace(part.TextPush) == false)) {
+                        _pushGatewayService.SendPushToContact(part.ContentItem, contactTitle);
+                        _notifier.Information(T("Push message sent to contact {0}.", contactTitle));
+                    }
                 }
             });
 
