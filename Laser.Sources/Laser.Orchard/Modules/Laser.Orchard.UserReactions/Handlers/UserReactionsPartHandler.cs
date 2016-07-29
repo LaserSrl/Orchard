48a61f002e4e17394dd95fbc7ca2d449355ac546
giuseppe.musso <giuseppe.musso@laser-group.com>
2016-07-29 14:36:17 +0200

    UserReactions fase 2: aggiunto warning relativo ai permessi in fase di modifica di un content type che contiene la UserReactionsPart.

--- Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.UserReactions/Handlers/UserReactionsPartHandler.cs
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.UserReactions/Handlers/UserReactionsPartHandler.cs
@@ -1,13 +1,10 @@
 ﻿using Orchard.Data;
 using Orchard.ContentManagement.Handlers;
 using Laser.Orchard.UserReactions.Models;
-using Orchard.ContentManagement.MetaData;
 
 
 namespace Laser.Orchard.UserReactions.Handlers {
     public class UserReactionsPartHandler : ContentHandler {
-        private readonly IContentDefinitionManager _contentDefinitionManager;
-
         public UserReactionsPartHandler(IRepository<UserReactionsPartRecord> repository) {
             Filters.Add(StorageFilter.For(repository));
         }
