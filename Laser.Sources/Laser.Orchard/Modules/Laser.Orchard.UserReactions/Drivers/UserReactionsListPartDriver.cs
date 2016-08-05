38ef25e685dd8567ef143e04cc1303cca660bd7f
luca.villa <luca.villa@laser-group.com>
2016-08-05 15:26:01 +0200

    - LV - Aggiunta validazione filtro, aggiunta controlli su dati inseriti

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.UserReactions/Drivers/UserReactionsListPartDriver.cs
@@ -0,0 +1,84 @@
+﻿using Laser.Orchard.UserReactions.Models;
+using Laser.Orchard.UserReactions.Services;
+using Orchard;
+using Orchard.ContentManagement.Drivers;
+using Orchard.ContentManagement;
+using Laser.Orchard.UserReactions.ViewModels;
+using Orchard.Localization;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Web.Script.Serialization;
+using System.Collections;
+using Orchard.ContentManagement.Handlers;
+
+namespace Laser.Orchard.UserReactions.Drivers {
+    public class UserReactionsListPartDriver : ContentPartDriver<UserReactionsPart> {
+
+        //richiamo il service per settaggio dati 
+        //Definisci una variabile settata alla interfaccia
+        private readonly IOrchardServices _orchardServices;
+        private readonly IUserReactionsService _userReactionService;
+
+        //Crea nel costruttore il settaggio alla var che esegue una select (quando si istanzia la classe si settano i dati nel costruttore) 
+        public UserReactionsListPartDriver(IUserReactionsService userReactionService, IOrchardServices orchardServices) {
+            _userReactionService = userReactionService;
+            _orchardServices = orchardServices;
+        }
+
+        public Localizer T { get; set; }
+
+
+
+
+
+        protected override DriverResult Display(UserReactionsPart part, string displayType, dynamic shapeHelper) {
+            //return ContentShape("Parts_Map",
+            //                    () => shapeHelper.Parts_Map(
+            //                          Longitude: part.Longitude,
+            //                          Latitude: part.Latitude));
+
+           List<UserReactionsTypeVM> reactionType = GetTypesTable();
+           return ContentShape("Parts_UserReactionsList", 
+                              () => shapeHelper.DisplayTemplate(TemplateName: "Parts/UserReactionsListForm", Model: reactionType, Prefix: Prefix));
+            
+            
+
+        }
+
+        //GET
+        //protected override DriverResult Editor(
+        //    MapPart part, dynamic shapeHelper) {
+        //    return ContentShape("Parts_Map_Edit",
+        //                        () => shapeHelper.EditorTemplate(
+        //                              TemplateName: "Parts/Map",
+        //                              Model: part));
+        //}
+
+        ////POST
+        //protected override DriverResult Editor(
+        //    MapPart part, IUpdateModel updater, dynamic shapeHelper) {
+        //    updater.TryUpdateModel(part, Prefix, null, null);
+        //    return Editor(part, shapeHelper);
+        //}
+
+
+            public List<UserReactionsTypeVM> GetTypesTable() {
+
+           
+            var reactionSettings = _orchardServices.WorkContext.CurrentSite.As<UserReactionsSettingsPart>();
+            var userRT = new UserReactionsTypes();
+
+            userRT.UserReactionsType = _userReactionService.GetTypesTable().Select(r => new UserReactionsTypeVM {
+                Id = r.Id,
+                TypeName = r.TypeName,
+
+            }).ToList();
+
+
+
+            return userRT.UserReactionsType;
+        }
+
+    }
+}
--- Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.UserReactions/Projections/UserReactionsQueryFilter.cs
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.UserReactions/Projections/UserReactionsQueryFilter.cs
@@ -1,4 +1,5 @@
 ﻿using Laser.Orchard.UserReactions.Models;
+using Orchard.ContentManagement;
 using Orchard.Data;
 using Orchard.Localization;
 using Orchard.Projections.Descriptors.Filter;
@@ -7,17 +8,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Razor.Tokenizer;
 
 namespace Laser.Orchard.UserReactions.Projections {
     public class UserReactionsQueryFilter : IFilterProvider {
         private readonly IRepository<UserReactionsSummaryRecord> _repoSummary;
         public Localizer T { get; set; }
+        private readonly ITokenizer _tokenizer;
 
-        public UserReactionsQueryFilter(IRepository<UserReactionsSummaryRecord> repoSummary) {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="repoSummary"></param>
+        public UserReactionsQueryFilter(IRepository<UserReactionsSummaryRecord> repoSummary){//, ITokenizer tokenizer 
             _repoSummary = repoSummary;
             T = NullLocalizer.Instance;
+            //_tokenizer = tokenizer;
         }
 
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="describe"></param>
         public void Describe(global::Orchard.Projections.Descriptors.Filter.DescribeFilterContext describe) {
             describe.For("Search", T("Search reactions"), T("Search reactions"))
                 .Element("ReactionsFilter", T("Reactions filter"), T("Filter for user reactions."),
@@ -27,39 +40,50 @@ namespace Laser.Orchard.UserReactions.Projections {
                 );
         }
 
-        public void ApplyFilter(FilterContext context) {
-            string query = context.State.SearchQuery;
-            List<int> ids = null;
-            int reaction = context.State.Reaction;
-            var op = (UserReactionsFieldOperator)Enum.Parse(typeof(UserReactionsFieldOperator), Convert.ToString(context.State.Operator));
-            int value = Convert.ToInt32(context.State.Value);
-            int min = Convert.ToInt32(context.State.Min);
-            int max = Convert.ToInt32(context.State.Max);
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        public void ApplyFilter(FilterContext context) {
+            string query =context.State.SearchQuery;
+           
+
+            List<int> ids = null;
+
+            string reaction = context.State.Reaction;
+            var op = (UserReactionsFieldOperator)Enum.Parse(typeof(UserReactionsFieldOperator), Convert.ToString(context.State.Operator));
+            int value = ((context.State.Value!=string.Empty) ? Convert.ToInt32(context.State.Value):0);
+            int min = ((context.State.Min!="")?Convert.ToInt32(context.State.Min):0);
+            int max = ((context.State.Max != "") ? Convert.ToInt32(context.State.Max) : 0);
+
+            
             switch (op) {
                 case UserReactionsFieldOperator.LessThan:
-                    ids = _repoSummary.Fetch(x => x.UserReactionsTypesRecord.Id == reaction && x.Quantity < value).Select(x => x.UserReactionsPartRecord.Id).ToList();
+                    ids = _repoSummary.Fetch(x => x.UserReactionsTypesRecord.TypeName == reaction && x.Quantity < value).Select(x => x.UserReactionsPartRecord.Id).ToList();
                     break;
                 case UserReactionsFieldOperator.LessThanEquals:
-                    ids = _repoSummary.Fetch(x => x.UserReactionsTypesRecord.Id == reaction && x.Quantity <= value).Select(x => x.UserReactionsPartRecord.Id).ToList();
+                    ids = _repoSummary.Fetch(x => x.UserReactionsTypesRecord.TypeName == reaction && x.Quantity <= value).Select(x => x.UserReactionsPartRecord.Id).ToList();
                     break;
                 case UserReactionsFieldOperator.Equals:
-                    ids = _repoSummary.Fetch(x => x.UserReactionsTypesRecord.Id == reaction && x.Quantity == value).Select(x => x.UserReactionsPartRecord.Id).ToList();
+                    ids = _repoSummary.Fetch(x => x.UserReactionsTypesRecord.TypeName == reaction && x.Quantity == value).Select(x => x.UserReactionsPartRecord.Id).ToList();
                     break;
                 case UserReactionsFieldOperator.NotEquals:
-                    ids = _repoSummary.Fetch(x => x.UserReactionsTypesRecord.Id == reaction && x.Quantity != value).Select(x => x.UserReactionsPartRecord.Id).ToList();
+                    ids = _repoSummary.Fetch(x => x.UserReactionsTypesRecord.TypeName == reaction && x.Quantity != value).Select(x => x.UserReactionsPartRecord.Id).ToList();
                     break;
                 case UserReactionsFieldOperator.GreaterThan:
-                    ids = _repoSummary.Fetch(x => x.UserReactionsTypesRecord.Id == reaction && x.Quantity > value).Select(x => x.UserReactionsPartRecord.Id).ToList();
+                    ids = _repoSummary.Fetch(x => x.UserReactionsTypesRecord.TypeName == reaction && x.Quantity > value).Select(x => x.UserReactionsPartRecord.Id).ToList();
                     break;
                 case UserReactionsFieldOperator.GreaterThanEquals:
-                    ids = _repoSummary.Fetch(x => x.UserReactionsTypesRecord.Id == reaction && x.Quantity >= value).Select(x => x.UserReactionsPartRecord.Id).ToList();
+                    ids = _repoSummary.Fetch(x => x.UserReactionsTypesRecord.TypeName == reaction && x.Quantity >= value).Select(x => x.UserReactionsPartRecord.Id).ToList();
                     break;
+
+
                 case UserReactionsFieldOperator.Between:
-                    ids = _repoSummary.Fetch(x => x.UserReactionsTypesRecord.Id == reaction && x.Quantity >= min && x.Quantity <= max).Select(x => x.UserReactionsPartRecord.Id).ToList();
+                    ids = _repoSummary.Fetch(x => x.UserReactionsTypesRecord.TypeName == reaction && x.Quantity >= min && x.Quantity <= max).Select(x => x.UserReactionsPartRecord.Id).ToList();
                     break;
                 case UserReactionsFieldOperator.NotBetween:
-                    ids = _repoSummary.Fetch(x => x.UserReactionsTypesRecord.Id == reaction && x.Quantity < min && x.Quantity > max).Select(x => x.UserReactionsPartRecord.Id).ToList();
+                    ids = _repoSummary.Fetch(x => x.UserReactionsTypesRecord.TypeName == reaction && x.Quantity < min && x.Quantity > max).Select(x => x.UserReactionsPartRecord.Id).ToList();
                     break;
             }
             if (ids.Count > 0) {
@@ -71,6 +95,13 @@ namespace Laser.Orchard.UserReactions.Projections {
             }
         }
 
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
         public LocalizedString DisplayFilter(FilterContext context) {
             return T("Content items having the specified reactions.");
         }
