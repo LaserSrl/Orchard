0473318c54b137dfdf1e53d351451f4c07d56643
giuseppe.musso <giuseppe.musso@laser-group.com>
2016-09-12 16:22:27 +0200

    Gateway pagamento: creata permission specifica per la configurazione dei POS.

--- Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.PaymentGateway/Navigation.cs
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.PaymentGateway/Navigation.cs
@@ -1,4 +1,5 @@
-﻿using Orchard.Localization;
+﻿using Laser.Orchard.PaymentGateway.Security;
+using Orchard.Localization;
 using Orchard.Security;
 using Orchard.UI.Navigation;
 using System;
@@ -21,8 +22,7 @@ namespace Laser.Orchard.PaymentGateway {
         public void GetNavigation(NavigationBuilder builder) {
             builder.Add(T("Settings"), menu => menu
                 .Add(T("Payment Gateway"), "10.0", subMenu => {
-                    //subMenu.LinkToFirstChild(true);
-                    subMenu.Action("Index", "Admin", new { area = "Laser.Orchard.PaymentGateway" }).Permission(StandardPermissions.SiteOwner);
+                    subMenu.Action("Index", "Admin", new { area = "Laser.Orchard.PaymentGateway" }).Permission(Permissions.ConfigurePayment);
                     subMenu.Add(T("Info"), "10.0", item => item.Action("Index", "Admin", new { area = "Laser.Orchard.PaymentGateway" }).LocalNav());
                     foreach (var pos in _posServices) {
                         subMenu.Add(T(pos.GetPosName()), "10.0", item => item.Action("Index", "Admin", new { area = pos.GetType().Namespace.Replace(".Services", "") }).LocalNav());
