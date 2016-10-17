7855e7e7241da2f87095304afeeb72965a9061ce
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2016-10-17 15:50:34 +0200

    ButtonToWorkflows: compila correttamente. HO fatto un paio di modifiche in ButtonToWorkflowsEvent.cs per levare dei warnings su delle ridefinizioni. Per testarne il funzioanmento bisogna prima integrare StartupConfig

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.ButtonToWorkflows/Migrations.cs
@@ -0,0 +1,46 @@
+using Orchard.ContentManagement.MetaData;
+using Orchard.Core.Contents.Extensions;
+using Orchard.Data.Migration;
+
+namespace Laser.Orchard.ButtonToWorkflows {
+    public class Migrations : DataMigrationImpl {
+        public int Create() {
+            ContentDefinitionManager.AlterPartDefinition("ButtonToWorkflowsPart", part => part.Attachable());
+            return 1;
+        }
+
+        public int UpdateFrom1() {
+            SchemaBuilder.CreateTable("ButtonToWorkflowsSettingsPartRecord", table => table
+                        .ContentPartRecord()
+                        .Column<string>("ButtonsText")
+                        .Column<string>("ButtonsAction")
+         );
+            return 2;
+        }
+        public int UpdateFrom2() {
+            SchemaBuilder.AlterTable("ButtonToWorkflowsSettingsPartRecord", table => table
+                        .AddColumn<string>("ButtonsDescription")
+                      
+         );
+            return 3;
+        }
+        public int UpdateFrom3() {
+            SchemaBuilder.AlterTable("ButtonToWorkflowsSettingsPartRecord", table => table
+                        .AddColumn<string>("ButtonsMessage")
+
+         );
+            return 4;
+
+            
+        }
+//        public int UpdateFrom4() {
+//            SchemaBuilder.AlterTable("ButtonToWorkflowsPart", table => table
+//                        .AddColumn<string>("ActionToExecute")
+//         );
+//            SchemaBuilder.AlterTable("ButtonToWorkflowsPart", table => table
+//             .AddColumn<string>("MessageToWrite")
+//);
+//            return 5;
+//        }
+    }
+}
