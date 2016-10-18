954ff251882d4f1eb670067a02c1186efc7cd07b
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2016-10-18 11:11:44 +0200

    Modulo GoogleAnalytics Compila e viene abilitato senza problemi.

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.GoogleAnalytics/Drivers/GoogleAnalyticsSettingsPartDriver.cs
@@ -0,0 +1,49 @@
+﻿using Orchard.ContentManagement;
+using Orchard.ContentManagement.Drivers;
+using Orchard.Localization;
+using Laser.Orchard.GoogleAnalytics.Models;
+using System;
+using Orchard.UI.Notify;
+using System.Web.Mvc;
+
+namespace GoogleAnalytics.Drivers {
+    public class GoogleAnalyticsSettingsPartDriver : ContentPartDriver<GoogleAnalyticsSettingsPart> {
+        private const string TemplateName = "Parts/GoogleAnalyticsSettings";
+        public Localizer T { get; set; }
+        private readonly INotifier _notifier;
+
+        public GoogleAnalyticsSettingsPartDriver(INotifier notifier) {
+            T = NullLocalizer.Instance;
+            _notifier = notifier;
+        }
+
+        
+        protected override string Prefix { get { return "GoogleAnalyticsSettings"; } }
+
+        //GET
+        protected override DriverResult Editor(GoogleAnalyticsSettingsPart part, dynamic shapeHelper) {
+            return ContentShape("Parts_GoogleAnalyticsSettings_Edit",
+                    () => shapeHelper.EditorTemplate(
+                        TemplateName: TemplateName,
+                        Model: part,
+                        Prefix: Prefix));
+        }
+
+        //POST
+        protected override DriverResult Editor(GoogleAnalyticsSettingsPart part, IUpdateModel updater, dynamic shapeHelper) {
+   
+                if (updater.TryUpdateModel(part, Prefix, null, null)) {
+                }
+                else {
+                    //foreach (var modelState in ModelState.Values) {
+                    //    foreach (var error in modelState.Errors) {
+                    //        Debug.WriteLine(error.ErrorMessage);
+                    //    }
+                    //}
+                    _notifier.Add(NotifyType.Error, T("Error on udpate google analytics"));
+                   
+                }
+            return Editor(part, shapeHelper);
+        }
+    }
+}
