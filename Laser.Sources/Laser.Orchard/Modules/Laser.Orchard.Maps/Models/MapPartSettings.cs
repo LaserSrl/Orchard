87f891eba67b8b066342df6b6d1846d55bc61290
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2016-10-18 13:04:45 +0200

    Integrato e testato maps

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.Maps/Models/MapPartSettings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laser.Orchard.Maps.Models {
+    public class MapPartSettings {
+        public bool Required { get; set; }
+    }
+}
