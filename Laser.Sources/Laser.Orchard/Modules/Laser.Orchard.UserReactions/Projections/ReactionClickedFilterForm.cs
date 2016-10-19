5178d0ddee24863f26a4f62f9dfcf4d93776af24
giuseppe.musso <giuseppe.musso@laser-group.com>
2016-10-19 17:31:00 +0200

    User Reactions: projection filter per i Contact che hanno cliccato su una specifica reaction di uno specifico contenuto.

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.UserReactions/Projections/ReactionClickedFilterForm.cs
@@ -0,0 +1,68 @@
+﻿using Laser.Orchard.UserReactions.Models;
+using Laser.Orchard.UserReactions.Services;
+using Orchard;
+using Orchard.ContentManagement;
+using Orchard.DisplayManagement;
+using Orchard.Forms.Services;
+using Orchard.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laser.Orchard.UserReactions.Projections {
+    public class ReactionClickedFilterForm : IFormProvider {
+        private readonly IUserReactionsService _reactionsService;
+        protected dynamic _shapeFactory { get; set; }
+        public Localizer T { get; set; }
+        public const string FormName = "ReactionClickedFilterForm";
+
+        public ReactionClickedFilterForm(IUserReactionsService reactionsService, IShapeFactory shapeFactory) {
+            _reactionsService = reactionsService;
+            _shapeFactory = shapeFactory;
+            T = NullLocalizer.Instance;
+        }
+
+        public void Describe(DescribeContext context) {
+            context.Form("ReactionClickedFilterForm", shape => {
+                var f = _shapeFactory.Form(
+                    Id: "ReactionClickedFilterForm",
+
+                     _Reaction: _shapeFactory.FieldSet(
+                            Id: "reaction",
+                            _Reaction: _shapeFactory.TextBox(
+                            Name: "Reaction",
+                            Title: T("Reaction type"),
+                            Classes: new[] { "tokenized" }
+                            )
+                        ),
+
+                    _ReactionTitle: _shapeFactory.Markup(
+                        Value: "<fieldset><legend>Reactions list available:</legend>"),
+
+                    _ReactionsList: _shapeFactory.List(
+                        Id: "reactionslist"
+                    ),
+
+                     _ReactionPanel: _shapeFactory.Markup(
+                        Value: " </fieldset>"),
+
+                    _FieldSetSingle: _shapeFactory.FieldSet(
+                        Id: "fieldset-content-item",
+                        _Value: _shapeFactory.TextBox(
+                            Id: "contentId", Name: "ContentId",
+                            Title: T("Content ID"),
+                            Classes: new[] { "tokenized" }
+                            )
+                        )
+                );
+
+                var reactionTypes = _reactionsService.GetTypesTableFiltered();
+                foreach (var item in reactionTypes) {
+                    f._ReactionsList.Add(item.TypeName); //, item.Id.ToString());
+                }
+                return f;
+            });
+        }
+    }
+}
---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.UserReactions/Projections/ReactionClickedQueryFilter.cs
@@ -0,0 +1,48 @@
+﻿using Orchard.ContentManagement;
+using Orchard.Data;
+using Orchard.Localization;
+using Orchard.Projections.Descriptors.Filter;
+using Orchard.Projections.Services;
+using Orchard.Users.Models;
+using NHibernate.Transform;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+namespace Laser.Orchard.UserReactions.Projections {
+    public class ReactionClickedQueryFilter : IFilterProvider {
+        public Localizer T { get; set; }
+
+        public ReactionClickedQueryFilter() {
+            T = NullLocalizer.Instance;
+        }
+        public void Describe(DescribeFilterContext describe) {
+            describe.For("Search", T("Search reactions"), T("Search reactions"))
+                .Element("Reaction Clicked Filter", T("Reaction clicked filter"), T("Filter for a specific reaction."),
+                    ApplyFilter,
+                    DisplayFilter,
+                    "ReactionClickedFilterForm"
+                );
+        }
+        public LocalizedString DisplayFilter(FilterContext context) {
+            return T("Users who clicked on a specific reaction.");
+        }
+        public void ApplyFilter(FilterContext context) {
+            string reaction = context.State.Reaction;
+            int contentId = ((context.State.ContentId != string.Empty) ? Convert.ToInt32(context.State.ContentId) : 0);
+            //context.Query.Join(a => a.ContentPartRecord<UserPartRecord>());
+            context.Query.ForType("CommunicationContact");
+            string subquery = string.Format(@"select contact.Id as contactId
+                from Laser.Orchard.CommunicationGateway.Models.CommunicationContactPartRecord as contact, 
+                Laser.Orchard.UserReactions.Models.UserReactionsClickRecord as click
+                where click.UserPartRecord.Id = contact.UserPartRecord_Id
+                and click.UserReactionsTypesRecord.TypeName='{0}'
+                and click.ContentItemRecordId={1}
+                group by contact.Id 
+                having mod(count(contact.Id) , 2) = 1", reaction.Replace("'", "''"), contentId);
+            context.Query.Where(a => a.Named("ci"), x => x.InSubquery("Id", subquery));
+        }
+    }
+}
