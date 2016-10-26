00cf2ddcc0f8067354c4dae546b84386febe86bf
marco.viglione <marco.viglione@laser-group.com>
2016-10-26 17:33:05 +0200

    Aggiunto modulo DevTools

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.DevTools/Permissions.cs
@@ -0,0 +1,38 @@
+﻿using Orchard.Environment.Extensions.Models;
+using Orchard.Security.Permissions;
+using System.Collections.Generic;
+
+namespace Laser.Orchard.DevTools {
+
+    public class Permissions : IPermissionProvider {
+        public static readonly Permission DevTools = new Permission { Description = "Enable user to devtools ", Name = "DevTools" };
+
+        public virtual Feature Feature { get; set; }
+
+        public IEnumerable<Permission> GetPermissions() {
+            return new[] {
+               DevTools
+            };
+        }
+
+        public IEnumerable<PermissionStereotype> GetDefaultStereotypes() {
+            return new[] {
+                new PermissionStereotype {
+                    Name = "Administrator",
+                },
+                new PermissionStereotype {
+                    Name = "Editor",
+                },
+                new PermissionStereotype {
+                    Name = "Moderator",
+                  },
+                new PermissionStereotype {
+                    Name = "Author",
+                },
+                new PermissionStereotype {
+                    Name = "Contributor",
+                },
+            };
+        }
+    }
+}
