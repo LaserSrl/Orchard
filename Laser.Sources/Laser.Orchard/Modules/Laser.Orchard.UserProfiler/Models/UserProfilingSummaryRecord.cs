08c3017c15719901c47bbb08e3634fa8bf50eced
HermesSbicego-Laser <hermes.sbicego@laser-group.com>
2017-01-25 16:26:27 +0100

    - User Profiler module generation

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.UserProfiler/Models/UserProfilingSummaryRecord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laser.Orchard.UserProfiler.Models {
+    public class UserProfilingSummaryRecord {
+        public virtual int Id { get; set; }
+        public virtual UserProfilingPartRecord UserProfilingPartRecord { get; set; }
+        public virtual string Text { get; set; }
+        public virtual TextSourceTypeOptions SourceType { get; set; }
+        public virtual int Count{ get; set; }
+    }
+}
