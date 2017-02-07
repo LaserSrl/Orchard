097d229825290fd2d2d95690d7f9a447c97ac319
giuseppe.musso <giuseppe.musso@laser-group.com>
2017-02-07 10:10:13 +0100

    Advanced search: aggiunto autocomplete al filtro per owner.

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.StartupConfig/Controllers/UsersAutocompleteController.cs
@@ -0,0 +1,34 @@
+﻿using Laser.Orchard.StartupConfig.Services;
+using Orchard;
+using Orchard.Security;
+using Orchard.Users.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laser.Orchard.StartupConfig.Controllers {
+    public class UsersAutocompleteController : Controller {
+        private readonly IUserSearchService _userSearchService;
+        public UsersAutocompleteController(IUserSearchService userSearchService) {
+            _userSearchService = userSearchService;
+        }
+        public ActionResult Search(string searchText) {
+            List<SearchResult> elenco = new List<SearchResult>();
+            foreach (var usr in _userSearchService.SearchByNameOrEmail(searchText)) {
+                elenco.Add(new SearchResult {
+                    UserName = usr.UserName,
+                    Email = usr.Email
+                });
+            }
+            return Json(elenco);
+        }
+        //**********************************************
+        // classe di utility per la creazione del risultato in formato json
+        public class SearchResult {
+            public string UserName { get; set; }
+            public string Email { get; set; }
+        }
+    }
+}
---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.StartupConfig/Services/IUserSearchService.cs
@@ -0,0 +1,24 @@
+﻿using Orchard;
+using Orchard.Security;
+using Orchard.Users.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laser.Orchard.StartupConfig.Services {
+    public interface IUserSearchService : IDependency {
+        IEnumerable<IUser> SearchByNameOrEmail(string searchText);
+    }
+    public class UserSearchService : IUserSearchService {
+        private readonly IOrchardServices _orchardServices;
+        public UserSearchService(IOrchardServices orchardServices) {
+            _orchardServices = orchardServices;
+        }
+        public IEnumerable<IUser> SearchByNameOrEmail(string searchText) {
+            var query = _orchardServices.ContentManager.Query().ForPart<UserPart>()
+                .Where<UserPartRecord>(u => u.UserName.Contains(searchText) || u.Email.Contains(searchText));
+            return (IEnumerable<IUser>)(query.List());
+        }
+    }
+}
