d8f4981b78fe660fd8a2049e7157fe556ae54f91
HermesSbicego-Laser <hermes.sbicego@laser-group.com>
2017-03-24 12:58:42 +0100

    - Solution - batch script to build with AnyCPU

--- Laser.Sources/Laser.Utilities/Build.Debug.bat
+++ Laser.Sources/Laser.Utilities/Build.Debug.bat
@@ -1,5 +1,5 @@
-MSBuild C:\Sviluppo\Laser.Platform.Orchard\Orchard.Sources\src\Orchard.sln /t:Rebuild /p:Configuration=Debug
+MSBuild C:\Sviluppo\Laser.Platform.Orchard\Orchard.Sources\src\Orchard.sln /t:Rebuild /p:Configuration="Release" /p:Platform="Any CPU"
 PAUSE
-MSBuild C:\Sviluppo\Laser.Platform.Orchard\Laser.Sources\Laser.Orchard\Laser.Orchard.sln /t:Rebuild /p:Configuration=Debug
+MSBuild C:\Sviluppo\Laser.Platform.Orchard\Laser.Sources\Laser.Orchard\Laser.Orchard.sln /t:Rebuild /p:Configuration="Release" /p:Platform="Any CPU"
 PAUSE
-DELETE C:\Sviluppo\Laser.Platform.Orchard\Orchard.Sources\src\Orchard.Web\App_Data\Dependencies\*.*
+DEL C:\Sviluppo\Laser.Platform.Orchard\Orchard.Sources\src\Orchard.Web\App_Data\Dependencies\*.*
