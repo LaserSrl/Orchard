670c7e1ba76ce51965401ac47fe2eb891271af13
diego.marguerettaz <diego.marguerettazi@laser-group.com>
2017-04-21 15:15:06 +0200

    fix della check box check-all per la selezione/deselezione di tutti i content nella pagina.

--- Laser.Sources/Laser.Orchard/Themes/KrakeAdmin/Scripts/admin.js
+++ Laser.Sources/Laser.Orchard/Themes/KrakeAdmin/Scripts/admin.js
@@ -72,7 +72,7 @@
     //});
 
     $(".check-all").change(function () {
-        $(this).parents("table.items").find(":checkbox").prop('checked', $(this).prop("checked"));
+        $("input[type=checkbox]:not(:disabled)").prop('checked', $(this).prop("checked"))
     });
 })(jQuery);
 
