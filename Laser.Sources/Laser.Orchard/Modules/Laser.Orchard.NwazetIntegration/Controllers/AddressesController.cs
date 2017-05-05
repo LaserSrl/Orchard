b24852277b8e11f975bc1c945013b96d14afae13
giuseppe.musso <giuseppe.musso@laser-group.com>
2017-05-05 09:27:36 +0200

    Nwazet integration: work in progress.

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.NwazetIntegration/Controllers/AddressesController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laser.Orchard.NwazetIntegration.Controllers {
+    public class AddressesController : Controller {
+        public ActionResult Index() {
+            return View("Index");
+        }
+    }
+}
