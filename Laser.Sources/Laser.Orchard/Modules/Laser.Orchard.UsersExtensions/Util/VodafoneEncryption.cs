8c72f796f88c0f375451a2f2efc9e215cd3584d1
giuseppe.musso <giuseppe.musso@laser-group.com>
2017-06-20 16:24:09 +0200

    IAmVodafone: prima versione per leggere gli header.

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.UsersExtensions/Util/VodafoneEncryption.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laser.Orchard.UsersExtensions.Util {
+    public class VodafoneEncryption {
+        public VodafoneEncryption() {
+
+        }
+        public string Decrypt(string encryptedText) {
+            return "";
+        }
+    }
+}
