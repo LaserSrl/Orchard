f84ba8e46f8d682f94863963c60600612fd1eddf
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2017-07-20 16:37:34 +0200

    CHanging the settings for an HiddenStringField starts a background update

--- Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.HiddenFields/ViewModels/HiddenStringFieldViewModels.cs
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.HiddenFields/ViewModels/HiddenStringFieldViewModels.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Laser.Orchard.HiddenFields.ViewModels {
 
@@ -12,13 +13,20 @@ namespace Laser.Orchard.HiddenFields.ViewModels {
 
         public HiddenStringFieldSettings Settings { get; set; }
 
-        public HiddenStringFieldUpdateProcessVariant ProcessVariant {get;set;}
+        public HiddenStringFieldUpdateProcessVariant ProcessVariant { get; set; }
+        public string ProcessVariantString {
+            get { return ProcessVariant.ToString(); }
+            set { ProcessVariant = (HiddenStringFieldUpdateProcessVariant)Enum.Parse(
+                    typeof(HiddenStringFieldUpdateProcessVariant), value); }
+        }
+
+        public IEnumerable<SelectListItem> ProcessVariants { get; set; }
     }
 
     public class HiddenStringFieldDriverViewModel {
 
         public bool IsEditAuthorized { get; set; }
-        
+
         public HiddenStringField Field { get; set; }
 
         public string Value { get; set; }
