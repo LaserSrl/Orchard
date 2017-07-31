25f6152103d338376d005bbe74ad4621851bb2bb
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2017-07-31 16:48:38 +0200

    Alcuni fix per gestire le questioni multilingua con i redirect della homepage.

--- Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.CulturePicker/Services/CulturePickerServices.cs
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.CulturePicker/Services/CulturePickerServices.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Orchard;
+using System;
 using System.Web;
-using Orchard;
 
 namespace Laser.Orchard.CulturePicker.Services {
     public class CulturePickerServices : ICulturePickerServices {
         private readonly IOrchardServices _orchardServices;
-       private readonly IWorkContextAccessor _workContextAccessor;
-        //public CulturePickerServices(IWorkContextAccessor workContextAccessor) {
-        //    _workContextAccessor = workContextAccessor;
-        //}
-       public CulturePickerServices(IOrchardServices orchardServices, IWorkContextAccessor workContextAccessor) {
+        private readonly IWorkContextAccessor _workContextAccessor;
+
+        public CulturePickerServices(IOrchardServices orchardServices, IWorkContextAccessor workContextAccessor) {
             _orchardServices = orchardServices;
             _workContextAccessor = workContextAccessor;
         }
@@ -24,7 +20,9 @@ namespace Laser.Orchard.CulturePicker.Services {
 
             //setting up domain for cookie allows to share it to sub-domains as well
             //if non-default port is used, we consider it as a testing environment without sub-domains
-            if (request.Url != null && request.Url.IsDefaultPort) {
+            if (request.Url != null && 
+                request.Url.IsDefaultPort &&
+                !(request.Url.Host.ToUpperInvariant() == "LOCALHOST")) {
                 // '.' prefix means, that cookie will be shared to sub-domains
                 cultureCookie.Domain = "." + request.Url.Host;
             }
