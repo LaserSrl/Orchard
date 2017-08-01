eb7fcac1bacd3eb6fbe0482c2d77f45a7598d70e
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2017-08-01 10:43:50 +0200

    aggiunti scripts per mantenere il cookie della cultura allineato ai contenuti della pagina visualizzata

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.CulturePicker/Drivers/LocalizationPartCookieScriptsDriver.cs
@@ -0,0 +1,24 @@
+﻿using Orchard.ContentManagement.Drivers;
+using Orchard.Localization.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.CompilerServices;
+
+namespace Laser.Orchard.CulturePicker.Drivers {
+    public class LocalizationPartCookieScriptsDriver : ContentPartDriver<LocalizationPart> {
+
+        protected override DriverResult Display(LocalizationPart part, string displayType, dynamic shapeHelper) {
+            if (displayType == "Detail") {
+                return ContentShape("Parts_Localization_CultureCookieScripts", () =>
+                    shapeHelper.Parts_Localization_CultureCookieScripts(Culture: GetCulture(part)));
+            }
+            return null;
+        }
+
+        private string GetCulture(LocalizationPart part) {
+            return part.Culture != null ? part.Culture.Culture : string.Empty;
+        }
+    }
+}
