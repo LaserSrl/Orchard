2a4e82b1629183bce6d738418c19aefe28c47039
marco.viglione <marco.viglione@laser-group.com>
2017-08-25 17:10:11 +0200

    Aggiunta la possibilità di disabilitare Google Analytics sulle pagine di front end.

--- Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.GoogleAnalytics/Services/MissingSettingsBanner.cs
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.GoogleAnalytics/Services/MissingSettingsBanner.cs
@@ -22,7 +22,7 @@ namespace Laser.Orchard.GoogleAnalytics.Services {
 
 		public IEnumerable<NotifyEntry> GetNotifications() {
 			var googleAnalyticsSettings = _orchardServices.WorkContext.CurrentSite.As<GoogleAnalyticsSettingsPart>();
-			if (googleAnalyticsSettings == null || string.IsNullOrWhiteSpace(googleAnalyticsSettings.GoogleAnalyticsKey)) {
+			if (googleAnalyticsSettings != null && string.IsNullOrWhiteSpace(googleAnalyticsSettings.GoogleAnalyticsKey) && (googleAnalyticsSettings.TrackOnFrontEnd || googleAnalyticsSettings.TrackOnAdmin)) {
                 var urlHelper = new UrlHelper(_orchardServices.WorkContext.HttpContext.Request.RequestContext);
                 var url = urlHelper.Action("Index", "Admin", new { Area = "Settings", groupInfoId = "Index" });
                 yield return new NotifyEntry { Message = T("The <a href=\"{0}#googleanalitycssetting\"> Google Analytics</a> settings need to be configured.",url), Type = NotifyType.Warning };
