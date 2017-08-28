d9546bd8f0162ffba5f06cc2571ca3dc94cba708
lorenzo.frediani <lorenzo.frediani@laser-group.com>
2017-08-28 17:54:46 +0200

    fix

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.AppDirect/Models/RequestState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Laser.Orchard.AppDirect.Models {
+    public enum RequestState {
+        [Display(Name = "To Create")]
+        ToCreate,
+        Created
+    }
+}
---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.AppDirect/ViewModels/ButtonVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laser.Orchard.AppDirect.ViewModels {
+    public class ButtonVM {
+        public string ButtonText { get; set; }
+        public string ButtonAction { get; set; }
+    }
+}
