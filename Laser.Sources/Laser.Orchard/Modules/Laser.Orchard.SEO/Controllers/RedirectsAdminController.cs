0f5d8ae4425f61a913f8b94c7bf46409e042b305
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2017-09-08 11:45:43 +0200

    CRUD for Redirect rules

--- Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.SEO/Controllers/RedirectsAdminController.cs
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.SEO/Controllers/RedirectsAdminController.cs
@@ -1,10 +1,13 @@
-﻿using Laser.Orchard.SEO.Services;
+﻿using Laser.Orchard.SEO.Models;
+using Laser.Orchard.SEO.Services;
 using Orchard;
 using Orchard.DisplayManagement;
 using Orchard.Environment.Extensions;
+using Orchard.Localization;
 using Orchard.Settings;
 using Orchard.UI.Admin;
 using Orchard.UI.Navigation;
+using Orchard.UI.Notify;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +22,8 @@ namespace Laser.Orchard.SEO.Controllers {
         private readonly IOrchardServices _orchardServices;
         private readonly ISiteService _siteService;
 
+        private readonly string[] _includeProperties = { "SourceUrl", "DestinationUrl", "IsPermanent" };
+
         private dynamic Shape { get; set; }
 
         public RedirectsAdminController(
@@ -31,7 +36,11 @@ namespace Laser.Orchard.SEO.Controllers {
             _orchardServices = orchardServices;
             _siteService = siteService;
             Shape = shapeFactory;
+
+            T = NullLocalizer.Instance;
         }
+
+        public Localizer T { get; set; }
 
         [HttpGet]
         public ActionResult Index(PagerParameters pagerParameters) {
@@ -43,6 +52,72 @@ namespace Laser.Orchard.SEO.Controllers {
                 .Redirects(items)
                 .Pager(pagerShape);
             return View((object)viewModel);
+        }
+
+        [HttpGet]
+        public ActionResult Add() {
+            return View();
+        }
+
+        [HttpPost, ActionName("Add")]
+        public ActionResult AddPost() {
+            var redirect = new RedirectRule();
+
+            //thanks to the _includeProperties, Update succeeds even if we are not adding the id from UI
+            if (!TryUpdateModel(redirect, _includeProperties)) {
+                _orchardServices.TransactionManager.Cancel();
+                return View(redirect);
+            }
+
+            return RedirectIfUrlsAreSame(redirect, (red) => _redirectService.Add(red));
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id) {
+            var redirect = _redirectService.GetRedirect(id);
+            if (redirect == null)
+                return HttpNotFound();
+
+            return View(redirect);
+        }
+
+        [HttpPost, ActionName("Edit")]
+        public ActionResult EditPost(int id) {
+            var redirect = _redirectService.GetRedirect(id);
+            if (redirect == null)
+                return HttpNotFound();
+
+            if (!TryUpdateModel(redirect, _includeProperties)) {
+                _orchardServices.TransactionManager.Cancel();
+                return View(redirect);
+            }
+
+            return RedirectIfUrlsAreSame(redirect, (red) => _redirectService.Update(red));
+        }
+        
+        private ActionResult RedirectIfUrlsAreSame(RedirectRule redirect, Action<RedirectRule> doOnSuccess) {
+            if (redirect.SourceUrl == redirect.DestinationUrl) {
+                ModelState.AddModelError("SourceUrl", T("Source url is equal to Destination url").Text);
+                _orchardServices.TransactionManager.Cancel();
+                return View(redirect);
+            }
+
+            doOnSuccess(redirect);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id) {
+            var redirect = _redirectService.GetRedirect(id);
+            if (redirect == null)
+                return HttpNotFound();
+
+            _redirectService.Delete(redirect);
+
+            _orchardServices.Notifier.Information(T("Redirect record was deleted"));
+
+            return RedirectToAction("Index");
         }
     }
 }
