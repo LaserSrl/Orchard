c726237be03bf94c477bd7a3b8fe624d07a3edd3
giuseppe.musso <giuseppe.musso@laser-group.com>
2017-09-28 14:36:31 +0200

    Policy: fix per il caso di utente che modifica le policy di un altro (per es. un contatto).

--- Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.Policy/Services/PolicyServices.cs
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.Policy/Services/PolicyServices.cs
@@ -177,14 +177,16 @@ namespace Laser.Orchard.Policy.Services {
                             _userPolicyAnswersRepository.Create(record);
                             _policyEventHandler.PolicyChanged(new PolicyEventViewModel {
                                 policyType = record.PolicyTextInfoPartRecord.PolicyType,
-                                accepted = record.Accepted
+                                accepted = record.Accepted,
+                                ItemPolicyPartRecordId = item.Id
                             });
                         } else if (record.Accepted != recordForHistory.Accepted) {
                             _userPolicyAnswersHistoryRepository.Create(recordForHistory);
                             _userPolicyAnswersRepository.Update(record);
                             _policyEventHandler.PolicyChanged(new PolicyEventViewModel {
                                 policyType = record.PolicyTextInfoPartRecord.PolicyType,
-                                accepted = record.Accepted
+                                accepted = record.Accepted,
+                                ItemPolicyPartRecordId = item.Id
                             });
                         }
                     }
@@ -194,7 +196,8 @@ namespace Laser.Orchard.Policy.Services {
                     _userPolicyAnswersRepository.Delete(record);
                     _policyEventHandler.PolicyChanged(new PolicyEventViewModel {
                         policyType = recordForHistory.PolicyTextInfoPartRecord.PolicyType,
-                        accepted = false
+                        accepted = false,
+                        ItemPolicyPartRecordId = item.Id
                     });
                 }
             }
