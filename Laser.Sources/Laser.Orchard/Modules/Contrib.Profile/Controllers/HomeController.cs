f4d6795735915626e090efa1c59e0d1bcb09c016
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2017-09-29 17:33:18 +0200

    Building the ContentItem to display at runtime. (not finished)

--- Laser.Sources/Laser.Orchard/Modules/Contrib.Profile/Controllers/HomeController.cs
+++ Laser.Sources/Laser.Orchard/Modules/Contrib.Profile/Controllers/HomeController.cs
@@ -7,6 +7,11 @@ using Orchard.Security;
 using Orchard.Themes;
 using Orchard.UI.Notify;
 using Contrib.Profile.Models;
+using Orchard.ContentManagement.MetaData.Models;
+using System.Linq;
+using Contrib.Profile.Settings;
+using System.Collections.Generic;
+using Orchard.ContentManagement.Handlers;
 
 namespace Contrib.Profile.Controllers {
     [ValidateInput(false), Themed]
@@ -28,13 +33,52 @@ namespace Contrib.Profile.Controllers {
         public ActionResult Index(string username) {
             IUser user = _membershipService.GetUser(username);
 
-            if(user == null || 
-                (user.As<ProfilePart>() == null || user.ContentItem.As<ProfilePart>() == null) ||
+            if(user == null ||
+                UserHasNoProfilePart(user) ||
                 !Services.Authorizer.Authorize(Permissions.ViewProfiles, user, null)) {
                 return HttpNotFound();
             }
 
-            dynamic shape = Services.ContentManager.BuildDisplay(user.ContentItem);
+            //based on the Type definition we find, we will build a new type definition
+            //that allows us to only show what we may.
+            var guid = Guid.NewGuid().ToString(); //used to generate type name
+            var viewTypeName = "UserView" + guid;
+            var userTypeDefinition = user.ContentItem.TypeDefinition;
+            var partDefinitions = new List<ContentTypePartDefinition>();
+            var parts = new List<ContentPart>();
+            foreach (var typePartDefinition in userTypeDefinition.Parts
+                .Where(ctpd => MayAllowPartDisplay(ctpd, userTypeDefinition.Name))) {
+
+                partDefinitions.Add(typePartDefinition);
+                parts.Add(user
+                    .ContentItem
+                    .Parts
+                    .Where(pa => pa.PartDefinition.Name == typePartDefinition.PartDefinition.Name)
+                    .FirstOrDefault());
+            }
+            parts.AddRange(
+                user.ContentItem.Parts
+                .Where(pa => !userTypeDefinition
+                    .Parts
+                    .Select(ctpd => ctpd.PartDefinition.Name)
+                    .Contains(pa.PartDefinition.Name))
+                );
+
+            var contentTypeDefinition = 
+                new ContentTypeDefinition(viewTypeName, viewTypeName,
+                partDefinitions, new SettingsDictionary());
+
+            var builder = new ContentItemBuilder(contentTypeDefinition);
+            foreach (var part in parts) {
+                builder.Weld(part);
+            }
+
+            //dynamic shape = Services.ContentManager.BuildDisplay(user.ContentItem);
+            dynamic shape = Services.ContentManager.BuildDisplay(builder.Build());
+
+            //foreach (var part in parts) {
+            //    user.ContentItem.Weld(part);
+            //}
 
             return View((object)shape);
         }
@@ -43,7 +87,7 @@ namespace Contrib.Profile.Controllers {
             IUser user = Services.WorkContext.CurrentUser;
 
             if (user == null ||
-                (user.As<ProfilePart>() == null || user.ContentItem.As<ProfilePart>() == null)) {
+                UserHasNoProfilePart(user)) {
                 return HttpNotFound();
             }
 
@@ -58,7 +102,7 @@ namespace Contrib.Profile.Controllers {
             IUser user = Services.WorkContext.CurrentUser;
 
             if (user == null ||
-                (user.As<ProfilePart>() == null || user.ContentItem.As<ProfilePart>() == null)) {
+                UserHasNoProfilePart(user)) {
                 return HttpNotFound();
             }
 
@@ -71,6 +115,15 @@ namespace Contrib.Profile.Controllers {
             Services.Notifier.Information(T("Your profile has been saved."));
 
             return RedirectToAction("Edit");
+        }
+
+        private bool UserHasNoProfilePart(IUser user) {
+            return user.As<ProfilePart>() == null && user.ContentItem.As<ProfilePart>() == null;
+        }
+
+        private bool MayAllowPartDisplay(ContentTypePartDefinition definition, string typeName) {
+            return definition.PartDefinition.Name == typeName || //this is to account for fields added to the type
+                definition.Settings.GetModel<ProfileFrontEndSettings>().AllowFrontEndDisplay;
         }
 
         bool IUpdateModel.TryUpdateModel<TModel>(TModel model, string prefix, string[] includeProperties, string[] excludeProperties) {
---
+++ Laser.Sources/Laser.Orchard/Modules/Contrib.Profile/Drivers/ProfilePartDriver.cs
@@ -0,0 +1,12 @@
+﻿using Contrib.Profile.Models;
+using Orchard.ContentManagement.Drivers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Contrib.Profile.Drivers {
+    //need a ContentPartDriver for a ContentPart to actually work
+    public class ProfilePartDriver : ContentPartDriver<ProfilePart> {
+    }
+}
