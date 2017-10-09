5971ced1bc89e118d4c10fbcd7fdd929339d7bac
lorenzo.frediani <lorenzo.frediani@laser-group.com>
2017-10-09 18:05:26 +0200

    fix

--- Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.AppDirect/Driver/AppDirectButtonDriver.cs
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.AppDirect/Driver/AppDirectButtonDriver.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Net;
 using Laser.Orchard.AppDirect.Models;
 using Laser.Orchard.AppDirect.Services;
 using Laser.Orchard.AppDirect.ViewModels;
@@ -9,19 +10,23 @@ using Orchard.ContentManagement;
 using Orchard.ContentManagement.Drivers;
 using Orchard.Data;
 using Orchard.Localization;
+using Orchard.Tasks.Scheduling;
 
 namespace Laser.Orchard.AppDirect.Driver {
     public class AppDirectButtonDriver : ContentPartDriver<AppDirectButtonPart> {
         private readonly IOrchardServices _orchardServices;
         private readonly IAppDirectCommunication _appDirectCommunication;
         private readonly IRepository<UserTenantRecord> _repoUserTenant;
+        private readonly IScheduledTaskManager _scheduledTaskManager;
         public AppDirectButtonDriver(IOrchardServices orchardServices,
             IAppDirectCommunication appDirectCommunication,
-            IRepository<UserTenantRecord> repoUserTenant) {
+            IRepository<UserTenantRecord> repoUserTenant,
+            IScheduledTaskManager scheduledTaskManager) {
             T = NullLocalizer.Instance;
             _orchardServices = orchardServices;
             _appDirectCommunication = appDirectCommunication;
             _repoUserTenant = repoUserTenant;
+            _scheduledTaskManager = scheduledTaskManager;
         }
         public Localizer T { get; set; }
         protected override string Prefix {
@@ -68,17 +73,32 @@ namespace Laser.Orchard.AppDirect.Driver {
                 button = GenerateButton(RequestState.ToCreate);
                 var AccountIdentifier = _orchardServices.WorkContext.HttpContext.Request.Form["Laser.Orchard.AppDirect.AppDirectUserPart.AccountIdentifier"];
                 if (!string.IsNullOrEmpty(AccountIdentifier)) {
-                    string outresponse;
-                    var data = "{\"success\":\"true\",\"accountIdentifier\":\"" + AccountIdentifier + "\"}";
-                    var uri = ((dynamic)part.ContentItem).AppDirectRequestPart.Uri.Value + "/result";
-                    if (_appDirectCommunication.MakeRequestToAppdirect(uri, Method.POST, data, _orchardServices.WorkContext.HttpContext.Request.Form["AppDirectRequestPart.ProductKey.Text"], out outresponse, "", "")) {
-                        button.ButtonAction = "";
-                        _appDirectCommunication.WriteEvent(EventType.Output, "Post async " + data + " " + outresponse + uri);
-                        part.ContentItem.As<AppDirectUserPart>().AccountIdentifier = AccountIdentifier;
-                        ((dynamic)part.ContentItem).AppDirectRequestPart.State.Value = RequestState.Created.ToString();
-                        ((dynamic)part.ContentItem).AppDirectRequestPart.Action.Value = "nothing";
-                        // part.ContentItem.As<TitlePart>().Title = RequestState.Created.ToString()+ " " + AccountIdentifier;
-                        _repoUserTenant.Create(new UserTenantRecord() { UuidCreator = part.ContentItem.As<AppDirectUserPart>().UuidCreator, Email = part.ContentItem.As<AppDirectUserPart>().Email, AccountIdentifier = AccountIdentifier, Product = _orchardServices.WorkContext.HttpContext.Request.Form["AppDirectRequestPart.ProductKey.Text"], TimeStamp = DateTime.UtcNow,Enabled=true });
+                    bool success = false;
+                    var request = (HttpWebRequest)WebRequest.Create("https://" + AccountIdentifier + "/OpenId/LogOn");
+                    request.Method = "HEAD";
+                    try {
+                        var response = (HttpWebResponse)request.GetResponse();
+                        success = response.StatusCode == HttpStatusCode.OK;
+                    }catch(Exception ex) {
+                        updater.AddModelError("NoIdentifierError", T(ex.Message) );
+                    }
+                    if (!success) {
+                        updater.AddModelError("NoIdentifierMessage", T("https://" + AccountIdentifier + "/OpenId/LogOn is not available"));
+                    }
+                    else {
+                        string outresponse;
+                        var data = "{\"success\":\"true\",\"accountIdentifier\":\"" + AccountIdentifier + "\"}";
+                        var uri = ((dynamic)part.ContentItem).AppDirectRequestPart.Uri.Value + "/result";
+                        if (_appDirectCommunication.MakeRequestToAppdirect(uri, Method.POST, data, _orchardServices.WorkContext.HttpContext.Request.Form["AppDirectRequestPart.ProductKey.Text"], out outresponse, "", "")) {
+                            button.ButtonAction = "";
+                            _appDirectCommunication.WriteEvent(EventType.Output, "Post async " + data + " " + outresponse + uri);
+                            part.ContentItem.As<AppDirectUserPart>().AccountIdentifier = AccountIdentifier;
+                            ((dynamic)part.ContentItem).AppDirectRequestPart.State.Value = RequestState.Created.ToString();
+                            ((dynamic)part.ContentItem).AppDirectRequestPart.Action.Value = "Created";
+                            // part.ContentItem.As<TitlePart>().Title = RequestState.Created.ToString()+ " " + AccountIdentifier;
+                            _repoUserTenant.Create(new UserTenantRecord() { UuidCreator = part.ContentItem.As<AppDirectUserPart>().UuidCreator, Email = part.ContentItem.As<AppDirectUserPart>().Email, AccountIdentifier = AccountIdentifier, Product = _orchardServices.WorkContext.HttpContext.Request.Form["AppDirectRequestPart.ProductKey.Text"], TimeStamp = DateTime.UtcNow, Enabled = true });
+                             _scheduledTaskManager.CreateTask("Laser.Orchard.AppDirect.ActionExecuted", DateTime.UtcNow.AddMinutes(1), part.ContentItem);
+                        }
                     }
                 }
                 else {
@@ -98,11 +118,13 @@ namespace Laser.Orchard.AppDirect.Driver {
                         _appDirectCommunication.WriteEvent(EventType.Output, "Post async " + data + " " + outresponse + uri);
                         //part.ContentItem.As<AppDirectUserPart>().AccountIdentifier = AccountIdentifier;
                         ((dynamic)part.ContentItem).AppDirectRequestPart.State.Value = RequestState.Cancelled.ToString();
-                        ((dynamic)part.ContentItem).AppDirectRequestPart.Action.Value = "nothing";
-                        var usertenant= _repoUserTenant.Fetch(x => x.Enabled == true && x.Email.Equals(part.ContentItem.As<AppDirectUserPart>().Email) && x.Product.Equals(_orchardServices.WorkContext.HttpContext.Request.Form["AppDirectRequestPart.ProductKey.Text"])).FirstOrDefault();
+                        ((dynamic)part.ContentItem).AppDirectRequestPart.Action.Value = "Cancelled";
+                        var usertenant = _repoUserTenant.Fetch(x => x.Enabled == true && x.Email.Equals(part.ContentItem.As<AppDirectUserPart>().Email) && x.Product.Equals(_orchardServices.WorkContext.HttpContext.Request.Form["AppDirectRequestPart.ProductKey.Text"])).FirstOrDefault();
                         if (usertenant != null)
                             usertenant.Enabled = false;
                         _repoUserTenant.Update(usertenant);
+                        _scheduledTaskManager.CreateTask("Laser.Orchard.AppDirect.ActionExecuted", DateTime.UtcNow.AddMinutes(1), part.ContentItem);
+
                     }
                 }
                 else {
@@ -122,7 +144,9 @@ namespace Laser.Orchard.AppDirect.Driver {
                         _appDirectCommunication.WriteEvent(EventType.Output, "Post async " + data + " " + outresponse + uri);
                         //part.ContentItem.As<AppDirectUserPart>().AccountIdentifier = AccountIdentifier;
                         ((dynamic)part.ContentItem).AppDirectRequestPart.State.Value = RequestState.Modified.ToString();
-                        ((dynamic)part.ContentItem).AppDirectRequestPart.Action.Value = "nothing";
+                        ((dynamic)part.ContentItem).AppDirectRequestPart.Action.Value = "Modified";
+                        _scheduledTaskManager.CreateTask("Laser.Orchard.AppDirect.ActionExecuted", DateTime.UtcNow.AddMinutes(1), part.ContentItem);
+
                     }
                 }
                 else {
@@ -142,10 +166,12 @@ namespace Laser.Orchard.AppDirect.Driver {
                         _appDirectCommunication.WriteEvent(EventType.Output, "Post async " + data + " " + outresponse + uri);
                         //part.ContentItem.As<AppDirectUserPart>().AccountIdentifier = AccountIdentifier;
                         ((dynamic)part.ContentItem).AppDirectRequestPart.State.Value = RequestState.AssignedUser.ToString();
-                        ((dynamic)part.ContentItem).AppDirectRequestPart.Action.Value = "nothing";
+                        ((dynamic)part.ContentItem).AppDirectRequestPart.Action.Value = "Assigned";
                         string subjectmail = ((dynamic)part.ContentItem).AppDirectRequestPart.PayloadSubject.Value;
                         ((dynamic)part.ContentItem).AppDirectRequestPart.State.Value = RequestState.AssignedUser.ToString();
-                        string jsonstring=((dynamic)part.ContentItem).AppDirectRequestPart.Request.Value;
+                        string jsonstring = ((dynamic)part.ContentItem).AppDirectRequestPart.Request.Value;
+                        _scheduledTaskManager.CreateTask("Laser.Orchard.AppDirect.ActionExecuted", DateTime.UtcNow.AddMinutes(1), part.ContentItem);
+
                         var json = JObject.Parse(jsonstring);
                         if ((json["payload"]).Type != JTokenType.Null) {
                             if ((json["payload"]["user"]).Type != JTokenType.Null) {
@@ -179,8 +205,9 @@ namespace Laser.Orchard.AppDirect.Driver {
                         _appDirectCommunication.WriteEvent(EventType.Output, "Post async " + data + " " + outresponse + uri);
                         //part.ContentItem.As<AppDirectUserPart>().AccountIdentifier = AccountIdentifier;
                         ((dynamic)part.ContentItem).AppDirectRequestPart.State.Value = RequestState.UnAssignedUser.ToString();
-                        ((dynamic)part.ContentItem).AppDirectRequestPart.Action.Value = "nothing";
-                        string subjectmail=((dynamic)part.ContentItem).AppDirectRequestPart.PayloadSubject.Value;
+                        ((dynamic)part.ContentItem).AppDirectRequestPart.Action.Value = "Unassigned";
+                        _scheduledTaskManager.CreateTask("Laser.Orchard.AppDirect.ActionExecuted", DateTime.UtcNow.AddMinutes(1), part.ContentItem);
+                        string subjectmail = ((dynamic)part.ContentItem).AppDirectRequestPart.PayloadSubject.Value;
                         var usertenant = _repoUserTenant.Fetch(x => x.Enabled == true && x.Email.Equals(subjectmail) && x.Product.Equals(_orchardServices.WorkContext.HttpContext.Request.Form["AppDirectRequestPart.ProductKey.Text"])).FirstOrDefault();
                         if (usertenant != null) {
                             usertenant.Enabled = false;
