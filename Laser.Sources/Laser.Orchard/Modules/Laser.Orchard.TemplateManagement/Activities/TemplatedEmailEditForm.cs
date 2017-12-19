81e5364ff4b51e2ee85c65bf0ca405dc33308613
giuseppe.musso <giuseppe.musso@laser-group.com>
2017-12-19 17:43:24 +0100

    VEC 2018: predisposizione traduzioni in inglese.

--- Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.TemplateManagement/Activities/TemplatedEmailEditForm.cs
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.TemplateManagement/Activities/TemplatedEmailEditForm.cs
@@ -101,12 +101,19 @@ namespace Laser.Orchard.TemplateManagement.Activities {
                         ),
                         _Parts: Shape.SelectList(
                             Id: "email-template", Name: "EmailTemplate",
-                            Title: T("Template"),
-                            Description: T("A template to format your email message."),
+                            Title: T("Default template"),
+                            Description: T("A default template to format your email message."),
                                 Size: 1,
                                 Multiple: false
-                                )
-                        );
+                        ),
+                        _Template: Shape.Textbox(
+                                Id: "custom-template-id",
+                                Name: "CustomTemplateId",
+                                Title: T("Custom template ID"),
+                                Description: T("Specify a template ID to format your email message. Leave blank to use the default template."),
+                                Classes: new[] { "large", "text", "tokenized" }
+                        )
+                    );
 
                     var allTemplates = _templateServices.GetTemplates().Where(w => !w.IsLayout);
 
