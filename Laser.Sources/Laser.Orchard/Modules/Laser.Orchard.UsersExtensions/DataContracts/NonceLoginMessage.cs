0d5db81ea5636f34f21388cc4ddfb74897bdf71a
giuseppe.musso <giuseppe.musso@laser-group.com>
2018-01-02 15:41:44 +0100

    VEC 2018: nuovi entry point per il login tramite nonce.

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.UsersExtensions/DataContracts/NonceLoginMessage.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace Laser.Orchard.UsersExtensions.DataContracts {
+    [DataContract]
+    public class NonceLoginMessage {
+        [DataMember(Name = "mail")]
+        public string Mail { get; set; }
+        [DataMember(Name = "nonce")]
+        public string Nonce { get; set; }
+    }
+}
