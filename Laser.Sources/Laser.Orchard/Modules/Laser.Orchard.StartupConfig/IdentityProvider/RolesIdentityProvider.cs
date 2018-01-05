ae257436ebbb9ab70361139a76a899b3b06688a4
giuseppe.musso <giuseppe.musso@laser-group.com>
2018-01-05 16:16:31 +0100

    Identity Provider: fix per RolesIdentityProvider.

--- Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.StartupConfig/IdentityProvider/RolesIdentityProvider.cs
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.StartupConfig/IdentityProvider/RolesIdentityProvider.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Web;
 
 namespace Laser.Orchard.StartupConfig.IdentityProvider {
-    public class RolesIdentityProvider {
+    public class RolesIdentityProvider : IIdentityProvider {
         public KeyValuePair<string, object> GetRelatedId(Dictionary<string, object> context) {
             var result = new KeyValuePair<string, object>("", 0);
             if (context.ContainsKey("Content")) {
