12ab29b33b411fedc26b2889374bedd5202430a2
lorenzo.frediani <lorenzo.frediani@laser-group.com>
2018-02-08 16:56:25 +0100

    Add Module

---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.Cache/Controllers/CacheUrlAdminController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Laser.Orchard.Cache.Models;
+using Laser.Orchard.Cache.Services;
+using Laser.Orchard.Cache.ViewModels;
+using Orchard;
+using Orchard.ContentManagement;
+using Orchard.Data;
+using Orchard.UI.Admin;
+using Orchard.UI.Navigation;
+
+namespace Laser.Orchard.Cache.Controllers {
+
+    public class CacheURLAdminController : Controller {
+        private readonly IOrchardServices _orchardServices;
+        private readonly IContentManager _contentManager;
+        private const string contentType = "CacheURL";
+        private readonly ICacheAliasServices _cacheAliasServices;
+        private IRepository<CacheUrlRecord> _cacheUrlRepository;
+
+        public CacheURLAdminController(
+            IContentManager contentManager,
+            IRepository<CacheUrlRecord> cacheUrlRepository,
+            IOrchardServices orchardServices,
+            ICacheAliasServices cacheAliasServices
+            ) {
+            _orchardServices = orchardServices;
+            _contentManager = contentManager;
+            _cacheUrlRepository = cacheUrlRepository;
+            _cacheAliasServices = cacheAliasServices;
+        }
+
+        [HttpGet]
+        [Admin]
+        public ActionResult Index() {
+            if (!_orchardServices.Authorizer.Authorize(Permissions.UrlCache)) {
+                return new HttpUnauthorizedResult();
+            }
+            var records = _cacheUrlRepository.Fetch(x => true).OrderBy(x => x.Priority).ToList();
+            return View("Index", new CacheUrlVM { Cached = records });
+        }
+
+        [HttpPost]
+        [Admin]
+        public ActionResult Index(CacheUrlVM vm) {
+            if (!_orchardServices.Authorizer.Authorize(Permissions.UrlCache)) {
+                return new HttpUnauthorizedResult();
+            }
+            foreach (var x in vm.Cached) {
+                if (x.Id == 0) {
+                    if (!string.IsNullOrEmpty(x.CacheURL)) {
+                        x.CacheURL = x.CacheURL.ToLower();
+                        _cacheUrlRepository.Create(x);
+                    }
+                }
+                else {
+                    if (string.IsNullOrEmpty(x.CacheURL)) {
+                        var old = _cacheUrlRepository.Get(x.Id);
+                        _cacheUrlRepository.Delete(old);
+                    }
+                    else {
+                        x.CacheURL = x.CacheURL.ToLower();
+                        _cacheUrlRepository.Update(x);
+                    }
+                }
+                _cacheUrlRepository.Flush();
+            }
+            _cacheAliasServices.RefreshCachedRouteConfig(_cacheUrlRepository);
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.Cache/Migration.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.ContentManagement.MetaData;
+using Orchard.Core.Contents.Extensions;
+using Orchard.Data.Migration;
+
+namespace Laser.Orchard.Cache {
+    public class Migration : DataMigrationImpl {
+        public int Create() {
+            return 1;
+        }
+        public int UpdateFrom1() {
+
+            return 2;
+        }
+        public int UpdateFrom2() {
+            SchemaBuilder.CreateTable("CacheUrlRecord",
+               table => table
+                .Column<int>("Id", column => column.PrimaryKey().Identity())
+                .Column<int>("CacheDuration", column => column.WithDefault(0))
+                .Column<int>("CacheGraceTime", column => column.WithDefault(0))
+                .Column<int>("Priority", column => column.WithDefault(0))
+                .Column<string>("CacheURL", column => column.WithLength(500))
+                .Column<string>("CacheToken", column => column.WithLength(500))
+           );
+            return 3;
+        }
+    }
+}
