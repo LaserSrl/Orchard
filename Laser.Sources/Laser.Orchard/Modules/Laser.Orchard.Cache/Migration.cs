a0fbe4a21e04c87afd28fab8fec6741afd74df1d
lorenzo.frediani <lorenzo.frediani@laser-group.com>
2018-02-08 17:09:24 +0100

    fix migration

--- Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.Cache/Migration.cs
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.Cache/Migration.cs
@@ -9,23 +9,17 @@ using Orchard.Data.Migration;
 namespace Laser.Orchard.Cache {
     public class Migration : DataMigrationImpl {
         public int Create() {
+            SchemaBuilder.CreateTable("CacheUrlRecord",
+              table => table
+               .Column<int>("Id", column => column.PrimaryKey().Identity())
+               .Column<int>("CacheDuration", column => column.WithDefault(0))
+               .Column<int>("CacheGraceTime", column => column.WithDefault(0))
+               .Column<int>("Priority", column => column.WithDefault(0))
+               .Column<string>("CacheURL", column => column.WithLength(500))
+               .Column<string>("CacheToken", column => column.WithLength(500))
+          );
             return 1;
         }
-        public int UpdateFrom1() {
 
-            return 2;
-        }
-        public int UpdateFrom2() {
-            SchemaBuilder.CreateTable("CacheUrlRecord",
-               table => table
-                .Column<int>("Id", column => column.PrimaryKey().Identity())
-                .Column<int>("CacheDuration", column => column.WithDefault(0))
-                .Column<int>("CacheGraceTime", column => column.WithDefault(0))
-                .Column<int>("Priority", column => column.WithDefault(0))
-                .Column<string>("CacheURL", column => column.WithLength(500))
-                .Column<string>("CacheToken", column => column.WithLength(500))
-           );
-            return 3;
-        }
     }
 }
