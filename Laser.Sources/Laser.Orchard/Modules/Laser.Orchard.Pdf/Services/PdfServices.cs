ae5f798a830994e6be6f328b5005c462e797d88f
lorenzo.frediani <lorenzo.frediani@laser-group.com>
2018-02-28 09:18:47 +0100

    Add Fonts to ItextSharp

--- Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.Pdf/Services/PdfServices.cs
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.Pdf/Services/PdfServices.cs
@@ -6,6 +6,7 @@ using System.IO;
 using Orchard.Logging;
 using Orchard.Localization;
 using Laser.Orchard.Pdf.Services.PageEvents;
+using System.Text;
 
 namespace Laser.Orchard.Pdf.Services {
     public interface IPdfServices : IDependency {
@@ -76,6 +77,12 @@ namespace Laser.Orchard.Pdf.Services {
                 using (var document = new Document(effectivePageSize, marginLeft, marginRight, marginTop, marginBottom)) {
                     using (var writer = PdfWriter.GetInstance(document, memoryStream)) {
                         writer.PageEvent = pdfPageEvent;
+                        int totalfonts = FontFactory.RegisterDirectory("%windir%\\Fonts");
+                        StringBuilder sb = new StringBuilder();
+                        foreach (string fontname in FontFactory.RegisteredFonts) {
+                            sb.Append(fontname + "\n");
+                        }
+                        writer.Add(new Paragraph("All Fonts:\n" + sb.ToString()));
                         using (var sr = new StringReader(html)) {
                             document.Open();
                             XMLWorkerHelper.GetInstance().ParseXHtml(writer, document, sr);
