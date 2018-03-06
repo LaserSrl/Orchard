f2dda988a56f8effef4cea73b59e55cabffb4034
giuseppe.musso <giuseppe.musso@laser-group.com>
2018-03-06 16:29:17 +0100

    JsonDataTable: aggiunto limite al numero di righe e display sul front-end.

--- Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.StartupConfig/Models/JsonDataTablePartSettings.cs
+++ Laser.Sources/Laser.Orchard/Modules/Laser.Orchard.StartupConfig/Models/JsonDataTablePartSettings.cs
@@ -1,7 +1,6 @@
 ﻿namespace Laser.Orchard.StartupConfig.Models {
     public class JsonDataTablePartSettings {
         private string _columnsDefinition = "[{field:'name',title:'Name'},{field:'description',title:'Description'}]"; // default value
-        private string _uniqueId = "id"; // default value
         public string ColumnsDefinition {
             get {
                 return _columnsDefinition;
@@ -12,16 +11,6 @@
                 }
             }
         }
-        public string UniqueId {
-            get {
-                return _uniqueId;
-            }
-            set {
-                if(string.IsNullOrWhiteSpace(value) == false) {
-                    _uniqueId = value;
-                }
-            }
-        }
-        public bool CardView { get; set; }
+        public int MaxRows { get; set; }
     }
 }
