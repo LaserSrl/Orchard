4d10bc16f1de3c5cad85697f3470f2cfc8e19199
HermesSbicego-Laser <hermes.sbicego@laser-group.com>
2019-05-22 12:46:39 +0200

    - Adjust Content permissions for Medias - Adjust Content permissions for ImageProfiles

--- src/Orchard.Web/Modules/Orchard.MediaLibrary/Security/MediaAuthorizationEventHandler.cs
+++ src/Orchard.Web/Modules/Orchard.MediaLibrary/Security/MediaAuthorizationEventHandler.cs
@@ -2,6 +2,7 @@
 using Orchard.MediaLibrary.Models;
 using Orchard.MediaLibrary.Services;
 using Orchard.Security;
+using Orchard.Security.Permissions;
 
 namespace Orchard.MediaLibrary.Security {
     public class MediaAuthorizationEventHandler : IAuthorizationServiceEventHandler {
@@ -21,6 +22,25 @@ namespace Orchard.MediaLibrary.Security {
         public void Adjust(CheckAccessContext context) {
             var mediaPart = context.Content.As<MediaPart>();
             if (mediaPart != null) {
+                Permission permission = context.Permission;
+                var typeDefinition = context.Content.ContentItem.TypeDefinition;
+                if (context.Permission == Core.Contents.Permissions.CreateContent) {
+                    permission = Permissions.EditMediaContent;
+                }
+                else if (context.Permission == Core.Contents.Permissions.EditContent || context.Permission == Core.Contents.Permissions.EditOwnContent) {
+                    permission = Permissions.EditMediaContent;
+                }
+                else if (context.Permission == Core.Contents.Permissions.PublishContent || context.Permission == Core.Contents.Permissions.PublishOwnContent) {
+                    permission = Permissions.EditMediaContent;
+                }
+                else if (context.Permission == Core.Contents.Permissions.DeleteContent || context.Permission == Core.Contents.Permissions.DeleteOwnContent) {
+                    permission = Permissions.DeleteMediaContent;
+                }
+                if (permission != context.Permission) {
+                    context.Granted = false; //Force granted to false so next adjust iteration will check against the new permission starting from an unauthorized condition
+                    context.Permission = permission;
+                    context.Adjusted = true;
+                }
                 context.Granted = _mediaLibraryService.CheckMediaFolderPermission(context.Permission, mediaPart.FolderPath);
             }
         }
---
+++ src/Orchard.Web/Modules/Orchard.MediaProcessing/Security/AuthorizationEventHandler.cs
@@ -0,0 +1,34 @@
+﻿using Orchard.ContentManagement;
+using Orchard.MediaProcessing.Models;
+using Orchard.Security;
+using Orchard.Security.Permissions;
+
+namespace Orchard.MediaProcessing.Security {
+    public class AuthorizationEventHandler : IAuthorizationServiceEventHandler {
+        public void Checking(CheckAccessContext context) { }
+        public void Complete(CheckAccessContext context) { }
+
+        public void Adjust(CheckAccessContext context) {
+            Permission permission = context.Permission;
+            if (context.Content.Is<ImageProfilePart>()) {
+                if (context.Permission == Core.Contents.Permissions.CreateContent) {
+                    permission = StandardPermissions.SiteOwner;
+                }
+                else if (context.Permission == Core.Contents.Permissions.EditContent || context.Permission == Core.Contents.Permissions.EditOwnContent) {
+                    permission = StandardPermissions.SiteOwner;
+                }
+                else if (context.Permission == Core.Contents.Permissions.PublishContent || context.Permission == Core.Contents.Permissions.PublishOwnContent) {
+                    permission = StandardPermissions.SiteOwner;
+                }
+                else if (context.Permission == Core.Contents.Permissions.DeleteContent || context.Permission == Core.Contents.Permissions.DeleteOwnContent) {
+                    permission = StandardPermissions.SiteOwner;
+                }
+            }
+            if (permission != context.Permission) {
+                context.Granted = false; //Force granted to false so next adjust iteration will check against the new permission starting from an unauthorized condition
+                context.Permission = permission;
+                context.Adjusted = true;
+            }
+        }
+    }
+}
