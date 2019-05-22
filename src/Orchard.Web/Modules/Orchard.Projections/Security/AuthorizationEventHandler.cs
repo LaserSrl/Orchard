92b765b7a63dd8636c879b5406441863ef400a9b
HermesSbicego-Laser <hermes.sbicego@laser-group.com>
2019-05-22 14:48:35 +0200

    - Adjust Content permissions for Queries

---
+++ src/Orchard.Web/Modules/Orchard.Projections/Security/AuthorizationEventHandler.cs
@@ -0,0 +1,35 @@
+﻿using Orchard.ContentManagement;
+using Orchard.Projections.Models;
+using Orchard.Security;
+using Orchard.Projections;
+using Orchard.Security.Permissions;
+
+namespace Orchard.Projections.Security {
+    public class AuthorizationEventHandler : IAuthorizationServiceEventHandler {
+        public void Checking(CheckAccessContext context) { }
+        public void Complete(CheckAccessContext context) { }
+
+        public void Adjust(CheckAccessContext context) {
+            Permission permission = context.Permission;
+            if (context.Content.Is<QueryPart>()) {
+                if (context.Permission == Core.Contents.Permissions.CreateContent) {
+                    permission = Permissions.ManageQueries;
+                }
+                else if (context.Permission == Core.Contents.Permissions.EditContent || context.Permission == Core.Contents.Permissions.EditOwnContent) {
+                    permission = Permissions.ManageQueries;
+                }
+                else if (context.Permission == Core.Contents.Permissions.PublishContent || context.Permission == Core.Contents.Permissions.PublishOwnContent) {
+                    permission = Permissions.ManageQueries;
+                }
+                else if (context.Permission == Core.Contents.Permissions.DeleteContent || context.Permission == Core.Contents.Permissions.DeleteOwnContent) {
+                    permission = Permissions.ManageQueries;
+                }
+            }
+            if (permission != context.Permission) {
+                context.Granted = false; //Force granted to false so next adjust iteration will check against the new permission starting from an unauthorized condition
+                context.Permission = permission;
+                context.Adjusted = true;
+            }
+        }
+    }
+}
