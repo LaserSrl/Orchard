eb90f5bd73ebf51440dd9be10c447ef7e1aa77e3
HermesSbicego-Laser <hermes.sbicego@laser-group.com>
2019-06-12 16:57:49 +0200

    - Manage Menus Permission Replaces ContentPermission if the content is a menu

--- src/Orchard.Web/Core/Navigation/Permissions.cs
+++ src/Orchard.Web/Core/Navigation/Permissions.cs
@@ -4,7 +4,21 @@ using Orchard.Security.Permissions;
 
 namespace Orchard.Core.Navigation {
     public class Permissions : IPermissionProvider {
-        public static readonly Permission ManageMenus = new Permission { Name = "ManageMenus", Description = "Manage all menus" };
+        public static readonly Permission ManageMenus = new Permission { Name = "ManageMenus", Description = "Manage all menus",
+            ReplaceFor = new PermissionReplaceContext {
+                ReplacedPermissions = new List<Permission> {
+                    Core.Contents.Permissions.CreateContent,
+                    Core.Contents.Permissions.EditContent,
+                    Core.Contents.Permissions.PublishContent,
+                    Core.Contents.Permissions.DeleteContent
+                },
+                Condition = (permission, content) => {
+                    string stereotype;
+                    return (content != null && (content.ContentItem.TypeDefinition.Settings.TryGetValue("Stereotype", out stereotype) && stereotype == "MenuItem" || content.ContentItem.ContentType == "Menu"));
+                },
+                OverrideSecurable = true
+            }
+        };
 
         public virtual Feature Feature { get; set; }
 
