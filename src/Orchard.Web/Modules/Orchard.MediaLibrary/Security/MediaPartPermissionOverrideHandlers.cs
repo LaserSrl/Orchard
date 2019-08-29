4fac2fe9761e37c7c2f2ab8e6b902a431b686bb2
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2019-08-29 16:02:44 +0200

    Permission override for mediaparts / medialibrary

---
+++ src/Orchard.Web/Modules/Orchard.MediaLibrary/Security/MediaPartPermissionOverrideHandlers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.Core.Contents.Security;
+using Orchard.MediaLibrary.Models;
+using Orchard.Security.Permissions;
+
+namespace Orchard.MediaLibrary.Security {
+    public class DeleteMediaContentPermissionOverrideHandlers
+        : PermissionOverrideAuthorizationEventHandler<MediaPart> {
+        protected override IEnumerable<Permission> ReplacedPermissions =>
+            new List<Permission>() {
+                Core.Contents.Permissions.DeleteContent,
+            };
+
+        protected override Permission OverridingPermission =>
+            Permissions.DeleteMediaContent;
+    }
+    public class EditMediaContentPermissionOverrideHandlers
+       : PermissionOverrideAuthorizationEventHandler<MediaPart> {
+        protected override IEnumerable<Permission> ReplacedPermissions =>
+            new List<Permission>() {
+                Core.Contents.Permissions.CreateContent,
+                Core.Contents.Permissions.EditContent,
+                Core.Contents.Permissions.PublishContent
+            };
+
+        protected override Permission OverridingPermission =>
+            Permissions.EditMediaContent;
+    }
+    public class ManageMediaContentPermissionOverrideHandlers
+       : PermissionOverrideAuthorizationEventHandler<MediaPart> {
+        protected override IEnumerable<Permission> ReplacedPermissions =>
+            new List<Permission>() {
+                Core.Contents.Permissions.CreateContent,
+                Core.Contents.Permissions.EditContent,
+                Core.Contents.Permissions.PublishContent,
+                Core.Contents.Permissions.DeleteContent,
+            };
+
+        protected override Permission OverridingPermission =>
+            Permissions.ManageMediaContent;
+    }
+}
