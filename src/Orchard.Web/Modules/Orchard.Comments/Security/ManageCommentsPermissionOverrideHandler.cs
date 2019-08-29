8177e08bb1c10470269bc1f2ba0c27f6e9c093bc
matteo.piovanelli <matteo.piovanelli@laser-group.com>
2019-08-29 14:38:59 +0200

    Permission override for comments: only users that have the ManageComments permission (the same used for moderation) are able to do any CRUD on comments on the backend.

---
+++ src/Orchard.Web/Modules/Orchard.Comments/Security/ManageCommentsPermissionOverrideHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Orchard.Comments.Models;
+using Orchard.Core.Contents.Security;
+using Orchard.Security.Permissions;
+
+namespace Orchard.Comments.Security {
+    public class ManageCommentsPermissionOverrideHandler
+        : PermissionOverrideAuthorizationEventHandler<CommentPart> {
+        protected override IEnumerable<Permission> ReplacedPermissions =>
+            new List<Permission>() {
+                Core.Contents.Permissions.CreateContent,
+                Core.Contents.Permissions.EditContent,
+                Core.Contents.Permissions.PublishContent,
+                Core.Contents.Permissions.DeleteContent,
+            };
+
+        protected override Permission OverridingPermission =>
+            Permissions.ManageComments;
+    }
+}
